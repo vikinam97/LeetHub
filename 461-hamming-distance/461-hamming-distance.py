f56f026b268e70340102779559920f4c845b25b5
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-07 00:24:56 +1000

    Time: 62 ms (9.34%), Space: 13.8 MB (60.30%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.py
+++ 461-hamming-distance/461-hamming-distance.py
@@ -1,10 +1,17 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         count = 0
-        while x != y:
-            if (x & 1) != (y & 1):
-                count += 1;
-            x = x >> 1
-            y = y >> 1
+        # solution 1         
+        # while x != y:
+        #     if (x & 1) != (y & 1):
+        #         count += 1;
+        #     x = x >> 1
+        #     y = y >> 1
+            
+        # solution 2 
+        x = x ^ y
+        while x != 0:
+            count += 1
+            x = x & (x-1)
             
         return count
