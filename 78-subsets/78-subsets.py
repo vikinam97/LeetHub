435f59c6643a05b4962bda765277addfd423e3ff
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-07 22:13:28 +1000

    Time: 49 ms (46.84%), Space: 14.1 MB (32.80%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,14 +1,28 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # Solution 1 -- using bit manipulation
-        superSet = []
-        for i in range(pow(2, len(nums))):
-            subSet = []
-            for bit in range(len(nums)):
-                if i & (1 << bit):
-                    subSet.append(nums[bit])
-            superSet.append(subSet)
-            
+        # Solution 2 using array
+        superSet = [[]]
+        
+        for num in nums:
+            setLen = len(superSet)
+            for idx in range(setLen):
+                temp = superSet[idx][:]
+                temp.append(num)
+                superSet.append(temp)
+        
         return superSet
+        
+        
+        # Solution 1 -- using bit manipulation
+        # Time -> O(N * 2 ^ N)
+        # Space -> O(2 ^ N)
+        # superSet = []
+        # for i in range(pow(2, len(nums))):
+        #     subSet = []
+        #     for bit in range(len(nums)):
+        #         if i & (1 << bit):
+        #             subSet.append(nums[bit])
+        #     superSet.append(subSet)
+        # return superSet
                 
             
