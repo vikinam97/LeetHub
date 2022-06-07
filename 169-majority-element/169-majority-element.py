24ed3527f1e49f3a217118e2ef6dd4c08f452747
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-07 16:45:52 +1000

    Time: 274 ms (25.48%), Space: 15.5 MB (85.40%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,30 +1,30 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # moores voting algorith find majority over n/2         
-#         meSoFar, count = None, 1
+        meSoFar, count = None, 1
         
-#         for num in nums:
-#             if meSoFar != num:
-#                 count -= 1
-#             else: 
-#                 count += 1
+        for num in nums:
+            if meSoFar != num:
+                count -= 1
+            else: 
+                count += 1
                 
-#             if count == 0:
-#                 meSoFar = num
-#                 count = 1
+            if count == 0:
+                meSoFar = num
+                count = 1
             
-#         return meSoFar
+        return meSoFar
 
         # Bit masking algo -> bit set if no of bits is > n/2
-        majElement = 0
-        for i in range(32):
-            setBitCount = 0
-            for num in nums:
-                if (num & (1 << i)):
-                    setBitCount += 1
-            if setBitCount > (len(nums)/2):
-                majElement = majElement | (1 << i)
+#         majElement = 0
+#         for i in range(32):
+#             setBitCount = 0
+#             for num in nums:
+#                 if (num & (1 << i)):
+#                     setBitCount += 1
+#             if setBitCount > (len(nums)/2):
+#                 majElement = majElement | (1 << i)
                 
-        return majElement
+#         return majElement
                 
                 
