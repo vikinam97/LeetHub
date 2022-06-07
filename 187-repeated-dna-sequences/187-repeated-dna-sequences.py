2cdab60197794b87483a56443fb1d12c4d31507f
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-07 23:54:04 +1000

    Time: 77 ms (77.07%), Space: 27.7 MB (33.50%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -1,14 +1,18 @@
 class Solution:
+
+    
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        subStrHash = {}
+        # Solution 1
+        # where N is length of string
+        # Time O(N ^ 2)
+        # Space O(N ^ 2)
         
+        subStrHash = {}
         for i in range(len(s) - 9):
             subStr = s[i:i+10]
             subStrHash[subStr] = subStrHash.get(subStr, 0) + 1
-        
         result = []
         for sstr in subStrHash:
             if subStrHash[sstr] > 1:
                 result.append(sstr)
-                
         return result
