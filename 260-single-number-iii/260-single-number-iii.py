e3f756e9f0843201c56b078f9a284df466320e96
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-07 21:55:41 +1000

    Time: 121 ms (24.20%), Space: 15.6 MB (83.02%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xorOfAll = 0;
+        for num in nums:
+            xorOfAll = xorOfAll ^ num
+        
+        firstSetBit = 0
+        while (xorOfAll & ( 1 << firstSetBit)) == 0:
+            firstSetBit += 1
+        
+        bitMask = 1 << firstSetBit
+        firstNumber, secondNumber = 0, 0
+        for num in nums:
+            if bitMask & num:
+                firstNumber ^= num
+            else: 
+                secondNumber ^= num
+                
+        return [firstNumber, secondNumber]
+            
+            
+            
