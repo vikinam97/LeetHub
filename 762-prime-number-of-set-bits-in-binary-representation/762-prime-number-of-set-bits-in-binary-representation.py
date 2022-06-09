73cd72cd2483b3f34615115b14372b63a40c2f56
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-09 23:37:08 +1000

    Time: 365 ms (66.25%), Space: 13.9 MB (75.76%) - LeetHub

--- 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.py
+++ 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.py
@@ -1,52 +1,16 @@
 class Solution:
-    # def isPrime(self, num):
-    #     if num <= 1:
-    #         return False
-    #     for i in range(2, num):
-    #         if num % i == 0:
-    #             return False
-    #     return True
-    def countPrimeSetBits(self, left: int, right: int) -> int:
-#         primes = 0
-        
-#         for i in range(left, right + 1):
-#             num = bin(i).count('1')
-#             if self.isPrime(num):
-#                 primes += 1
-#         return primes
-
-        
-        resCount  = 0
-        primeSet = {1: False}
-        def isPrime(num):
-            if num <= 1:
+    def isPrime(self, num):
+        if num <= 1:
+            return False
+        for i in range(2, num):
+            if num % i == 0:
                 return False
-            for i in range(2, num):
-                if num % i == 0:
-                    return False
-            return True
-#             if primeSet.get(n) != None:
-#                 return primeSet.get(n)
-#             for i in range(2, n//2):
-#                 if n % i == 0:
-#                     primeSet[n] = False
-#                     return False
-                
-#             primeSet[n] = True
-#             return True
-        
-        def countSetBit(n):
-            count = 0
-            while n != 0:
-                count += 1
-                n = n & (n-1)
-            return count
-        
-        for num in range(left, right+1):
-            noSetBit = countSetBit(num)
-            print(num, " => ", noSetBit, isPrime(noSetBit))
-            if isPrime(noSetBit) == True:
-                resCount += 1
-                
-        return resCount
+        return True
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        primes = 0
+        for i in range(left, right + 1):
+            num = bin(i).count('1')
+            if self.isPrime(num):
+                primes += 1
+        return primes
         
