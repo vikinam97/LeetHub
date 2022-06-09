7b12a3458c2285ea6a1cd3c9ae018aa4533d5e5e
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-09 15:51:12 +1000

    Time: 208 ms (33.30%), Space: 15.1 MB (11.40%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,25 +1,40 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
-        def binarySearch(idx, subTarget):
-            i, j = idx + 1, len(numbers) - 1
-            while i <= j:
-                mid = (i + j) // 2
-                print(i, j, mid);
-
-                if numbers[mid] == subTarget:
-                    return mid
-                elif numbers[mid] > subTarget:
-                    j = mid - 1
-                else:
-                    i = mid + 1
-            return None
-        
-        for i in range(len(numbers)):
-            subTarget = target - numbers[i]
-            tempNumIdx = binarySearch(i, subTarget)
-            if tempNumIdx != None and numbers[tempNumIdx] == subTarget:
-                return [i+1, tempNumIdx+1]
-            
+        # Solution 2
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            tempSum = numbers[i] + numbers[j]
+            if tempSum == target:
+                return [i+1, j+1]
+            elif tempSum > target:
+                j -= 1
+            else:
+                i += 1
         return None
+                
+        
+        # Solution 1
+        # Time O(NlogN)
+        # Space O(1)
+#         def binarySearch(idx, subTarget):
+#             i, j = idx + 1, len(numbers) - 1
+#             while i <= j:
+#                 mid = (i + j) // 2
+#                 print(i, j, mid);
+
+#                 if numbers[mid] == subTarget:
+#                     return mid
+#                 elif numbers[mid] > subTarget:
+#                     j = mid - 1
+#                 else:
+#                     i = mid + 1
+#             return None
+        
+#         for i in range(len(numbers)):
+#             subTarget = target - numbers[i]
+#             tempNumIdx = binarySearch(i, subTarget)
+#             if tempNumIdx != None and numbers[tempNumIdx] == subTarget:
+#                 return [i+1, tempNumIdx+1]
+            
+#         return None
             
