2e78ca99bf4febd533b7c5f95f603bebbdbc5628
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-13 19:46:38 +0530

    Added solution - LeetHub

---
+++ Prime Number - GFG/prime-number.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+class Solution:
+    def isPrime (self, N):
+        if N <= 1:
+            return 0
+        
+        i = 2
+        while i * i <= N:
+            if N % i == 0:
+                return 0
+            i += 1
+                
+        return 1
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+       
+
+        ob = Solution()
+        print(ob.isPrime(N))
+# } Driver Code Ends
