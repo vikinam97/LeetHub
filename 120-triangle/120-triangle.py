578ddc868f01982455d7a902989691c6ec9602b0
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-13 13:03:13 +0530

    Time: 111 ms (33.14%), Space: 15.1 MB (37.66%) - LeetHub

---
+++ 120-triangle/120-triangle.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        prev = [float('inf')] * len(triangle)
+        
+        curr = [float('inf')] * len(triangle)
+        
+        prev[0] = triangle[0][0]
+        
+        for row in range(0, len(triangle)-1):
+            for col in range(len(triangle[row])):
+                num = triangle[row+1][col]
+                num2 = triangle[row+1][col+1]
+                curr[col] = min(curr[col], num + prev[col])
+                curr[col+1] = min(curr[col+1], num2 + prev[col])
+            
+            prev = curr
+            curr = [float("inf")] * len(triangle)
+        
+        
+        print(prev)
+        
+        minSum = float("inf")
+        for i in range(len(prev)):
+            minSum = min(minSum, prev[i])
+        
+        return minSum
+        
+        
