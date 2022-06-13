cb9d390c3289a4b103f1c72e94bff2675d1ffdbf
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-13 19:24:28 +0530

    Time: 7234 ms (20.76%), Space: 67.8 MB (41.66%) - LeetHub

--- 204-count-primes/204-count-primes.py
+++ 204-count-primes/204-count-primes.py
@@ -1,27 +1,48 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        
-        if n <= 1:
+        if n <= 2:
             return 0
+
+        def SieveOfEratosthenes(n):
+            prime = [True for i in range(n + 1)]
+            prime[0], prime[1] = False, False  # 0 and 1 aren't primes
+
+            p = 2
+            while p * p <= n:
+                if prime[p] == True:
+                    for i in range(p * 2, n + 1, p):
+                        prime[i] = False
+                p += 1
+
+            numberOfPrimes = 0
+            for p in range(n):
+                if prime[p]:
+                    numberOfPrimes += 1
+            return numberOfPrimes
+
+        return SieveOfEratosthenes(n)
         
-        striverList = [True] * n
+#         if n <= 1:
+#             return 0
         
-        striverList[0] = striverList[1] = False
+#         striverList = [True] * n
         
-        for i in range(2, math.isqrt(n) + 1):
-            if striverList[i] == False:
-                continue
-            j = i * i
-            while j < n:
-                striverList[j] = False
-                j += i
+#         striverList[0] = striverList[1] = False
+        
+#         for i in range(2, math.isqrt(n) + 1):
+#             if striverList[i] == False:
+#                 continue
+#             j = i * i
+#             while j < n:
+#                 striverList[j] = False
+#                 j += i
             
-        count = 0
-        for i in range(2, n):
-            if striverList[i] == True:
-                count += 1
+#         count = 0
+#         for i in range(2, n):
+#             if striverList[i] == True:
+#                 count += 1
         
-        return count
+#         return count
             
                 
         
