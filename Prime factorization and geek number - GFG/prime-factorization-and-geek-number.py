45b3661dd5a785e8ebf67097a3b87959d9d7fbc1
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-14 20:05:29 +0530

    Added solution - LeetHub

---
+++ Prime factorization and geek number - GFG/prime-factorization-and-geek-number.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+class Solution:
+    def geekNumber(self, N):
+        # code here
+        
+        i = 2
+        while i * i <= N:
+            count = 0
+            while N % i == 0:
+                count += 1
+                N = N / i
+            if count > 1:
+                return 0
+            i += 1
+        return 1
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        
+        ob = Solution()
+        if ob.geekNumber(N)==1 :
+            print("Yes");
+        else :
+            print("No");
+# } Driver Code Ends
