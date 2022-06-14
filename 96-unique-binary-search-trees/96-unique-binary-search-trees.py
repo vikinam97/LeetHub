791ab5b73ff3a26cfc647d643579098d4b1b1f5b
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-14 17:42:15 +0530

    Time: 48 ms (35.35%), Space: 13.9 MB (57.54%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,27 +1,28 @@
 class Solution:
     def numTrees(self, n: int) -> int:
+        # Solution - Dynamic programming
+        # Time - O(N ^ 2)
+        # Space - O(N)
+#         dp = [0] * (n + 1)
+#         dp[0] = dp[1] = 1
         
-        dp = [0] * (n + 1)
+#         for i in range(2, n + 1):
+#             dp[i] = 0
+#             for j in range(i):
+#                 dp[i] += dp[j] * dp[i-1-j]
         
-        dp[0] = dp[1] = 1
-        
-        for i in range(2, n + 1):
-            dp[i] = 0
-            for j in range(i):
-                dp[i] += dp[j] * dp[i-1-j]
-        
-        return dp[n]
+#         return dp[n]
             
         
         
         # Solution recurrence with memorization
         # Time O(N ^2)
         # space O(N) memo and call stack
-        # self.memo = {
-        #     0: 1,
-        #     1: 1
-        # }
-        # return self.recHepler(n)
+        self.memo = {
+            0: 1,
+            1: 1
+        }
+        return self.recHepler(n)
     
     def recHepler(self, n):
         if self.memo.get(n, None):
