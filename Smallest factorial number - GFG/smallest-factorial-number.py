c61617d175834fa01eb2ceeb402bf94d72cd3786
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-15 12:06:30 +0530

    Added solution - LeetHub

--- Smallest factorial number - GFG/smallest-factorial-number.py
+++ Smallest factorial number - GFG/smallest-factorial-number.py
@@ -11,35 +11,35 @@ class Solution:
         
     def findNum(self, n : int):
         
-        low=0
-        high=n*5
-        ans=low
-        while(low<=high):
-            mid=(low+high)//2
-            zeros = self.countTrailZero(mid)
-            if(zeros>=n):
-                ans=mid
-                high=mid-1
-            else:
-                low=mid+1
-
-        return ans
-        
-        # l, r = 0, 5 * n
-        # ans = l
-        # while l <= r:
-            
-        #     mid = (l+ r) // 2
-            
-        #     trailZeroCount = self.countTrailZero(mid)
-            
-        #     if trailZeroCount > n:
-        #         ans = mid
-        #         r = mid - 1
+        # low=0
+        # high=n*5
+        # ans=low
+        # while(low<=high):
+        #     mid=(low+high)//2
+        #     zeros = self.countTrailZero(mid)
+        #     if(zeros>=n):
+        #         ans=mid
+        #         high=mid-1
         #     else:
-        #         l = mid + 1
-        
+        #         low=mid+1
+
         # return ans
+        
+        l, r = 0, 5 * n
+        ans = l
+        while l <= r:
+            
+            mid = (l+ r) // 2
+            
+            trailZeroCount = self.countTrailZero(mid)
+            
+            if trailZeroCount >= n:
+                ans = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        
+        return ans
 
 #{ 
 #  Driver Code Starts
