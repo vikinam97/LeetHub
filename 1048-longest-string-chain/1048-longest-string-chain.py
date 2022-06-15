f76d7ab52b7dbf785b32ef03445e749f57ed33bb
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-15 10:15:46 +0530

    Time: 1777 ms (16.31%), Space: 14.5 MB (48.74%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,20 +1,4 @@
-class Solution:
-    def cmprStr(self, w1, w2): 
-        i, j = 0, 0
-        flag = 0
-        while i < len(w1) and j < len(w2):
-            # print(w1[i], w2[j])
-            if w1[i] != w2[j]:
-                if flag == 1:
-                    return False
-                flag = 1
-                i += 1
-                continue
-            i += 1
-            j += 1            
-
-        return w1[i-1] == w2[j-1]
-        
+class Solution:        
     def longestStrChain(self, words: List[str]) -> int:
         
         def sortLen(wrd):
@@ -29,13 +13,25 @@ class Solution:
                 if len(words[i]) != len(words[j]) + 1:
                     continue
                 if self.cmprStr(words[i], words[j]):
-                    # print(words[i], words[j])
                     dp[i] = max(dp[i], dp[j] + 1)
                     maxSoFar = max(maxSoFar, dp[i])
         
-        
-        
         return maxSoFar
+    
+    def cmprStr(self, w1, w2): 
+        i, j = 0, 0
+        flag = 0
+        while i < len(w1) and j < len(w2):
+            if w1[i] != w2[j]:
+                if flag == 1:
+                    return False
+                flag = 1
+                i += 1
+                continue
+            i += 1
+            j += 1            
+
+        return w1[i-1] == w2[j-1]
         
         
         
