89e312a94b20c06bd4544c12b84e6fb02fe0c92c
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-16 20:31:15 +0530

    Time: 255 ms (11.93%), Space: 16.2 MB (6.65%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -9,23 +9,17 @@ class Solution:
         for i in range(1, len(height)):
             leftMax[i] = max(height[i], leftMax[i-1])
         
+        rightMax[len(height)-1] = height[len(height)-1]
         # find the right max of the heights
+        for i in range(len(height) - 2, 0 - 1, -1):
+            rightMax[i] = max(height[i], rightMax[i+1])
+        
         # to find the water above
         waterList = [0] * len(height)
-        for i in range(len(height) - 1, 0 - 1, -1):
-            if i == len(height) - 1:
-                rightMax[i] = height[i] 
-            else:
-                rightMax[i] = max(height[i], rightMax[i+1])
+        for i in range(len(height)):
             waterLevel = min(rightMax[i], leftMax[i])
             if waterLevel > height[i]:
                 waterList[i] = waterLevel - height[i]
-        
-        
-        # for i in range(len(height)):
-        #     waterLevel = min(rightMax[i], leftMax[i])
-        #     if waterLevel > height[i]:
-        #         waterList[i] = waterLevel - height[i]
         
         return sum(waterList)
             
