310470a5d72751174339fa1ea97511c1b6ca873a
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-16 13:44:13 +0530

    Time: 86 ms (66.72%), Space: 15.1 MB (49.41%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,5 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Solution - using hash map
+        # Time O(N)
+        # Space O(N)
         hashMap = {}
         for i in range(len(nums)):
             hashMap[nums[i]] = i
