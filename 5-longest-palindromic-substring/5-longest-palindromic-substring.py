6e8bcbba71144a218abd740c689bc7c9d00efbc8
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-16 10:55:18 +0530

    Time: 1912 ms (33.15%), Space: 14.1 MB (29.29%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,6 +1,5 @@
 class Solution:
     def expandPalindrome(self, i, j, s):
-        
         while i >= 0 and j < len(s) and s[i] == s[j]:
             pMax = j - i + 1
             if self.maxSoFar < pMax:
@@ -9,6 +8,7 @@ class Solution:
                 self.jmax = j
             i -= 1
             j += 1
+            
     def longestPalindrome(self, s: str) -> str:
         self.maxSoFar = 0
         self.imax, self.jmax = 0, 0
@@ -17,26 +17,10 @@ class Solution:
             # odd palindrromes
             i, j = idx - 1, idx + 1
             self.expandPalindrome(i, j, s)
-            # while i >= 0 and j < len(s) and s[i] == s[j]:
-            #     pMax = j - i + 1
-            #     if maxSoFar < pMax:
-            #         maxSoFar = pMax
-            #         imax = i
-            #         jmax = j
-            #     i -= 1
-            #     j += 1
             
             # even palindromes
             i, j = idx, idx + 1
             self.expandPalindrome(i, j, s)
-            # while i >= 0 and j < len(s) and s[i] == s[j]:
-            #     pMax = j - i + 1
-            #     if maxSoFar < pMax:
-            #         maxSoFar = pMax
-            #         imax = i
-            #         jmax = j
-            #     i -= 1
-            #     j += 1
         
         return s[self.imax:self.jmax+1]
             
