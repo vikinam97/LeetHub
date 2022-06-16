0ab3582dad655ad0a5b6b787dc2a5fe360dcffc7
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-16 20:46:49 +0530

    Time: 1990 ms (10.90%), Space: 24.9 MB (85.16%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        h = float('inf')
+        profit = 0
+        for ind, value in enumerate(prices):
+            h = min(value, h)
+            profit = max(profit, value-h)
+        return profit 
         # Solution - kadane algo
         # Time O(N)
         # Space O(N)
-        dp = [0] * len(prices)
-        maxSoFar = 0
+#         dp = [0] * len(prices)
+#         maxSoFar = 0
         
-        dp[len(prices) - 1] = prices[len(prices) - 1]
-        for i in range(len(prices) - 2, 0 - 1, -1):
-            dp[i] = max(dp[i+1], prices[i])
+#         dp[len(prices) - 1] = prices[len(prices) - 1]
+#         for i in range(len(prices) - 2, 0 - 1, -1):
+#             dp[i] = max(dp[i+1], prices[i])
             
-            maxSoFar = max(maxSoFar, dp[i] - prices[i])
+#             maxSoFar = max(maxSoFar, dp[i] - prices[i])
             
-        return maxSoFar
+#         return maxSoFar
             
         
