b339a95344f3ec5be4a1b46fd1282d9a43bc0c5a
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-16 10:54:53 +0530

    Time: 1221 ms (62.82%), Space: 14 MB (59.59%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,31 +1,44 @@
 class Solution:
+    def expandPalindrome(self, i, j, s):
+        
+        while i >= 0 and j < len(s) and s[i] == s[j]:
+            pMax = j - i + 1
+            if self.maxSoFar < pMax:
+                self.maxSoFar = pMax
+                self.imax = i
+                self.jmax = j
+            i -= 1
+            j += 1
     def longestPalindrome(self, s: str) -> str:
-        maxSoFar = 0
-        imax, jmax = 0, 0
+        self.maxSoFar = 0
+        self.imax, self.jmax = 0, 0
+                
         for idx in range(len(s)):
             # odd palindrromes
             i, j = idx - 1, idx + 1
-            while i >= 0 and j < len(s) and s[i] == s[j]:
-                pMax = j - i + 1
-                if maxSoFar < pMax:
-                    maxSoFar = pMax
-                    imax = i
-                    jmax = j
-                i -= 1
-                j += 1
+            self.expandPalindrome(i, j, s)
+            # while i >= 0 and j < len(s) and s[i] == s[j]:
+            #     pMax = j - i + 1
+            #     if maxSoFar < pMax:
+            #         maxSoFar = pMax
+            #         imax = i
+            #         jmax = j
+            #     i -= 1
+            #     j += 1
             
             # even palindromes
             i, j = idx, idx + 1
-            while i >= 0 and j < len(s) and s[i] == s[j]:
-                pMax = j - i + 1
-                if maxSoFar < pMax:
-                    maxSoFar = pMax
-                    imax = i
-                    jmax = j
-                i -= 1
-                j += 1
+            self.expandPalindrome(i, j, s)
+            # while i >= 0 and j < len(s) and s[i] == s[j]:
+            #     pMax = j - i + 1
+            #     if maxSoFar < pMax:
+            #         maxSoFar = pMax
+            #         imax = i
+            #         jmax = j
+            #     i -= 1
+            #     j += 1
         
-        return s[imax:jmax+1]
+        return s[self.imax:self.jmax+1]
             
             
                 
