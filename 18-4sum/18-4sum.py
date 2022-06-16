3ee894f554ec6a237775ab10f6a5a385a907540e
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-16 12:52:48 +0530

    Time: 1535 ms (39.85%), Space: 14.1 MB (15.95%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -1,48 +1,36 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-	
-        def kSum(nums: List[int], target: int, k: int) -> List[List[int]]:
-            res = []
-            
-            # If we have run out of numbers to add, return res.
-            if not nums:
-                return res
-            
-            # There are k remaining values to add to the sum. The 
-            # average of these values is at least target // k.
-            average_value = target // k
-            
-            # We cannot obtain a sum of target if the smallest value
-            # in nums is greater than target // k or if the largest 
-            # value in nums is smaller than target // k.
-            if average_value < nums[0] or nums[-1] < average_value:
-                return res
-            
-            if k == 2:
-                return twoSum(nums, target)
-    
-            for i in range(len(nums)):
-                if i == 0 or nums[i - 1] != nums[i]:
-                    for subset in kSum(nums[i + 1:], target - nums[i], k - 1):
-                        res.append([nums[i]] + subset)
-    
-            return res
-
-        def twoSum(nums: List[int], target: int) -> List[List[int]]:
-            res = []
-            s = set()
-    
-            for i in range(len(nums)):
-                if len(res) == 0 or res[-1][1] != nums[i]:
-                    if target - nums[i] in s:
-                        res.append([target - nums[i], nums[i]])
-                s.add(nums[i])
-    
-            return res
-
+        L = len(nums)
+        ans = []
+        if L<4:
+            return ans
         nums.sort()
-        return kSum(nums, target, 4)
-
+        print(nums)
+        for i in range(L-3):
+            a = nums[i]
+            if ans and a == ans[-1][0]:
+                continue
+            for j in range(i+1, L-2):
+                b = nums[j]
+                if ans and a == ans[-1][0] and b ==ans[-1][1]:
+                    continue
+                start = j+1
+                end = L-1
+                sub_target = target-a-b
+                while start<end:
+                    if nums[start]+nums[end] == sub_target:
+                        if ans and b==ans[-1][1] and nums[start] == ans[-1][2] and nums[end]==ans[-1][-1]:
+                            start+=1
+                            end-=1      
+                            continue
+                        ans.append([a,b,nums[start], nums[end]])
+                        start+=1
+                        end-=1
+                    elif nums[start]+nums[end] > sub_target:
+                        end-=1
+                    else:
+                        start+=1
+        return ans
 # class Solution:
 #     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
 #         sumDict = defaultdict(list)
