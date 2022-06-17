d2a9fc27fe86424de4638817f2856fbe0b6c1487
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-17 19:36:55 +0530

    Time: 171 ms (22.74%), Space: 14.3 MB (24.84%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,7 +1,9 @@
 class Solution:
     def findMedianSortedArrays(self, A: List[int], B: List[int]) -> float:
         
-        # A, B = nums1, nums2
+        # Solution - binary search to partition list
+        # Time O(log(min(A, B)))
+        # Space O(1)
         
         totalSize = len(A) + len(B)
         half = totalSize // 2
@@ -17,10 +19,10 @@ class Solution:
             i = (l + r) // 2
             j = half - (i+1) - 1
             
-            Aleft = A[i] if i >= 0 else float('-inf')
-            Aright = A[i + 1] if (i+1) < len(A) else float("inf")
-            Bleft = B[j] if j >= 0 else float('-inf')
-            Bright = B[j + 1] if (j+1) < len(B) else float('inf')
+            Aleft = A[i] if i >= 0 else float('-infinity')
+            Aright = A[i + 1] if (i+1) < len(A) else float("infinity")
+            Bleft = B[j] if j >= 0 else float('-infinity')
+            Bright = B[j + 1] if (j+1) < len(B) else float('infinity')
             
             if Aleft <= Bright and Bleft <= Aright:
                 if totalSize % 2 == 0:
@@ -31,32 +33,6 @@ class Solution:
                 r = i - 1
             else:
                 l = i + 1
-#         if len(A)>len(B): A, B = B, A
-        
-#         total = len(A)+len(B)
-#         half = total//2
-#         l = 0
-#         r = len(A)-1
-        
-#         while True:
-#             i = l + (r-l)//2
-#             j = half-(i+1)-1
-            
-            # Aleft = A[i] if i>=0 else float('-inf')
-            # Aright = A[i+1] if i+1<len(A) else float('inf')
-            # Bleft = B[j] if j>=0 else float('-inf')
-            # Bright = B[j+1] if j+1<len(B) else float('inf')
-            
-#             if Aleft<=Bright and Bleft<=Aright:
-                # if total%2==0:
-                #     return (max(Aleft, Bleft)+min(Aright, Bright))/2
-                # else:
-                #     return min(Aright, Bright)
-#             elif Aleft>Bright:
-#                 r = i-1
-#             else:
-#                 l = i+1
-                
             
             
             
