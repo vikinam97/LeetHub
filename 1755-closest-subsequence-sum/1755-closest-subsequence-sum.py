4cdccbd04932c8c25553fece4633e19a2a96bbb5
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-17 21:40:22 +0530

    Time: 6330 ms (14.48%), Space: 128.2 MB (73.79%) - LeetHub

--- 1755-closest-subsequence-sum/1755-closest-subsequence-sum.py
+++ 1755-closest-subsequence-sum/1755-closest-subsequence-sum.py
@@ -1,6 +1,8 @@
 class Solution:
     def minAbsDifference(self, nums: List[int], goal: int) -> int:
-        
+        # Solution - 2 set binary serach
+        # Time - O(2^(N/2) * log(2^(N/2))
+        # Space O(2^n)
         def generateSubsetSumList(st, ed):
             result = [0]
             for i in range(st, ed+1):
@@ -8,20 +10,6 @@ class Solution:
                 for j in range(prevLen):
                     result.append(result[j] + nums[i])
             return result
-        
-#         def searchBinary(arr, target):
-#             l, r = 0, len(arr) - 1
-#             while l < r:
-#                 mid = (l + r) // 2
-                
-#                 if arr[mid] == target:
-#                     return arr[mid]
-#                 elif arr[mid] > target:
-#                     r = mid - 1
-#                 else:
-#                     l = mid + 1
-            
-#             return arr[l]
         
         list1 = generateSubsetSumList(0, (len(nums)) // 2)
         list2 = generateSubsetSumList((len(nums)) // 2 + 1, len(nums) - 1)
@@ -32,26 +20,16 @@ class Solution:
         result = float('infinity')
         
         for i in range(len(list1)):
-            t = goal - list1[i]
-            i = bisect_left(list2, t)
+            subTarget = goal - list1[i]
+            i = bisect_left(list2, subTarget)
             
             if i < len(list2):
-                result = min(result, abs(t - list2[i]))
+                closeSoFar = min(closeSoFar, abs(subTarget - list2[i]))
                 
             if i > 0:
-                result = min(result, abs(t - list2[i-1]))
-            
-            
-#             subTarget = goal - list1[i]
-#             ps = searchBinary(list2, subTarget)
-#             print(list1[i], subTarget, ps)
-            
-#             diff = abs(goal - (ps + list1[i]))
-#             if diff == 0:
-#                 return 0
-#             closeSoFar = min(closeSoFar, diff)
+                closeSoFar = min(closeSoFar, abs(subTarget - list2[i-1]))
         
-        return result
+        return closeSoFar
             
         
         
