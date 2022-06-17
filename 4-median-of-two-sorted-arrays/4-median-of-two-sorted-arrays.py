d364bffe4fdc766a5f4908a7ed35368573759e0a
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-17 19:29:41 +0530

    Time: 138 ms (47.22%), Space: 14.2 MB (24.84%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,13 +1,32 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    def findMedianSortedArrays(self, A: List[int], B: List[int]) -> float:
+        if len(A)>len(B): A, B = B, A
         
-        nums1 = [*nums1, *nums2]
+        total = len(A)+len(B)
+        half = total//2
+        l = 0
+        r = len(A)-1
         
-        nums1.sort()
+        while True:
+            i = l + (r-l)//2
+            j = half-(i+1)-1
+            
+            Aleft = A[i] if i>=0 else float('-inf')
+            Aright = A[i+1] if i+1<len(A) else float('inf')
+            Bleft = B[j] if j>=0 else float('-inf')
+            Bright = B[j+1] if j+1<len(B) else float('inf')
+            
+            if Aleft<=Bright and Bleft<=Aright:
+                if total%2==0:
+                    return (max(Aleft, Bleft)+min(Aright, Bright))/2
+                else:
+                    return min(Aright, Bright)
+            elif Aleft>Bright:
+                r = i-1
+            else:
+                l = i+1
+                
+            
+            
+            
         
-        mid = (len(nums1)-1) // 2
-        
-        if len(nums1) & 1:
-            return nums1[mid]
-        else:
-            return (nums1[mid] + nums1[mid+1]) / 2
