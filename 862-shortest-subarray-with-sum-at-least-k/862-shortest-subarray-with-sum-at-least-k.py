1091daba278a415fb3280b851d85ee1f1c1f3825
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-17 16:12:42 +0530

    Time: 1960 ms (39.67%), Space: 28.7 MB (24.19%) - LeetHub

--- 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.py
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.py
@@ -1,50 +1,35 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
+        slidingSum = 0
+        minSoFar = float('Inf')
         
-        d = collections.deque([(-1,0)])
-        sums = 0
-        res = len(nums) + 1
-
-        for i,n in enumerate(nums):
-            sums += n
-            while d and sums-d[0][1] >= k:
-                res = min(res,i-d.popleft()[0])        
-            # else:
-            while d and sums <= d[-1][1]:
-                    d.pop()
-            d.append((i,sums))
-        if res == len(nums)+1:
-            return -1
-        else:
-            return res
+        monoq = collections.deque([(0, -1)])
         
-        
-#         slidingSum = 0
-#         minSoFar = float('Inf')
-        
-#         monoq = collections.deque()
-        
-#         for i in range(len(nums)):
+        for i in range(len(nums)):
+            
 #             slidingSum += nums[i]
-#             minSoFar = min(minSoFar, i+1)
             
-#             # sum, index
-#             lastPop = [float('Inf'), float('Inf')]
-            
-#             # minimize the window
-#             while monoq and (slidingSum - monoq[0][0]) >= k:
-#                 lastPop = monoq.popLeft()
+#             while monoq and slidingSum-monoq[0][0] >= k:
+#                 popped = monoq.popleft()
+#                 minSoFar = min(minSoFar,i-popped[1])   
                 
-#             if lastPop[0] != float('Inf'):
-#                 minSoFar = min(minSoFar, i - lastPop[1])
-                
-#             while monoq and monoq[0][0] < slidingSum:
-#                 monoq.popleft()
+#             while monoq and sums <= monoq[-1][0]:
+#                     monoq.pop()
+                    
+#             monoq.append((slidingSum, i))
             
-#             monoq.append([slidingSum, i])
+            slidingSum += nums[i]
+            
+            while monoq and (slidingSum - monoq[0][0]) >= k:
+                popped = monoq.popleft()
+                minSoFar = min(minSoFar, i - popped[1])
+                
+            while monoq and slidingSum <= monoq[-1][0]:
+                monoq.pop()
+            
+            monoq.append((slidingSum, i))
         
-        
-#         return minSoFar
+        return minSoFar if minSoFar != float('Inf') else -1
         
         
         
