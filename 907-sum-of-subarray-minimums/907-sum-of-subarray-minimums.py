33343322219dd75e847550f8a765eab0c5bb0882
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-18 17:33:30 +0530

    Time: 488 ms (89.93%), Space: 18.7 MB (89.67%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
@@ -1,11 +1,14 @@
 class Solution:
-    def sumSubarrayMins(self, A: List[int]) -> int:
-        A.append(-1)
+    def sumSubarrayMins(self, nums: List[int]) -> int:
+        # Solution - monotonic stack
+        # Time - O(N)
+        # Space - O(N)
+        nums.append(-1)
         stack=[-1]
         res=0
-        for i in range(len(A)):
-            while stack and A[i]<A[stack[-1]]:
+        for i in range(len(nums)):
+            while nums[i]<nums[stack[-1]]:
                 idx=stack.pop()
-                res+=A[idx]*(i-idx)*(idx-stack[-1])
+                res+=nums[idx]*(i-idx)*(idx-stack[-1])
             stack.append(i)
         return res%(10**9+7)
