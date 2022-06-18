5660167a69c3d36c8da79221b995891e9f2d432f
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-18 15:23:26 +0530

    Time: 113 ms (68.69%), Space: 17.7 MB (60.37%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.py
@@ -1,5 +1,7 @@
 class NumArray:
-
+    # Solution - prefix sum
+    # Time - O(N + Q) Q = number of query
+    # Space - O(N)
     def __init__(self, nums: List[int]):
         self.prefixSum = [0] * len(nums)
         self.prefixSum[0] = nums[0]
@@ -8,7 +10,6 @@ class NumArray:
 
     def sumRange(self, left: int, right: int) -> int:
         leftSum = self.prefixSum[left-1] if left > 0 else 0 
-        
         return self.prefixSum[right] - leftSum
 
 
