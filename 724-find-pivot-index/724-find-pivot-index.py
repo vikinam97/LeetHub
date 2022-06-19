16d395114e7374dd8fa642ae011790ddd927a3cc
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-19 15:04:15 +0530

    Time: 296 ms (19.00%), Space: 15.2 MB (92.54%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -1,17 +1,28 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        leftSum = [0] * len(nums)
-        rightSum = [0] * len(nums)
-        
-        leftSum[0] = nums[0]
-        rightSum[len(nums)-1] = nums[len(nums)-1]
-        lth = len(nums)
-        for i in range(lth-2, -1, -1):
-            rightSum[i] = rightSum[i+1] + nums[i]
-        for i in range(lth):
-            leftSum[i] = leftSum[i-1] + nums[i]
-            if leftSum[i] == rightSum[i]:
+        # Solution - prefix sum
+        # Time O(N)
+        # Space O(1)
+        total = sum(nums)
+        leftsum = 0
+        for i, x in enumerate(nums):
+            if leftsum == (total - leftsum - x):
                 return i
-        
-        
+            leftsum += x
         return -1
+        
+        # Solution - prefix and suffix sum
+        # Time O(N)
+        # Space O(N)
+        # leftSum = [0] * len(nums)
+        # rightSum = [0] * len(nums)
+        # leftSum[0] = nums[0]
+        # rightSum[len(nums)-1] = nums[len(nums)-1]
+        # lth = len(nums)
+        # for i in range(lth-2, -1, -1):
+        #     rightSum[i] = rightSum[i+1] + nums[i]
+        # for i in range(lth):
+        #     leftSum[i] = leftSum[i-1] + nums[i]
+        #     if leftSum[i] == rightSum[i]:
+        #         return i
+        # return -1
