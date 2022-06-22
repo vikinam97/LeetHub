ad9a554c9549866052d8ac1396803297939ef0c1
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-22 19:12:54 +0530

    Time: 1315 ms (12.82%), Space: 28.7 MB (17.31%) - LeetHub

--- 2274-maximum-consecutive-floors-without-special-floors/2274-maximum-consecutive-floors-without-special-floors.py
+++ 2274-maximum-consecutive-floors-without-special-floors/2274-maximum-consecutive-floors-without-special-floors.py
@@ -3,13 +3,10 @@ class Solution:
         special.sort()
         maxSoFar = special[0] - bottom
         
-        print(special)
-        
         for i in range(1, len(special)):
             a = (special[i-1]+1)
             b = (special[i]-1)
             diff = (b - a) + 1
-            # print(a, b, a - b, diff)
             maxSoFar = max(maxSoFar, diff)
         
         diffAtTop = top - special[-1] 
