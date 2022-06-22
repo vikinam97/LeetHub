3852a2ea93a63ee5f57a2997f99a6c2a03c7ae54
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-22 09:28:31 +0530

    Time: 2123 ms (9.15%), Space: 19.7 MB (10.10%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,30 +1,29 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        
+                # Solution - Quick Select Algo
+        # Time - O(N)
+        # Space - O(log(k)) recursive stack
         def partition(start, end):
-            if start >= end:
-                return nums[start]
-            
-            i = start - 1
-            j = start
+            i, j = start, end
             pivot = end
             
-            while j < end:
-                if nums[j] < nums[pivot]:
+            while i < j:
+                if nums[i] < nums[pivot]:
                     i += 1
-                    nums[i], nums[j] = nums[j], nums[i]
-                    j += 1
-                else:
-                    j += 1
+                    continue
+                if nums[j] >= nums[pivot]:
+                    j -= 1
+                    continue
+                nums[i], nums[j] = nums[j], nums[i]
+                
+            nums[i], nums[pivot] = nums[pivot], nums[i]
             
-            nums[i+1], nums[pivot] = nums[pivot], nums[i+1]
-            if (i+1) == (len(nums) - k):
-                return nums[i + 1]
+            # selecting next apth
+            if i == len(nums) - k:
+                return nums[i]
+            elif i > len(nums) - k:
+                return partition(start, i-1)
             
-            if (i+1) > (len(nums) - k):
-                return partition(start, i)
-            
-            return partition(i+2, end)
-        
-        return partition(0, len(nums)-1)
+            return partition(i+1, end)
+        return partition(0, len(nums) -1)
         
