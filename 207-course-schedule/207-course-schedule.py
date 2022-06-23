4e6c1bcb05852ed9e6e167cf3f3f4cd2a9e182f6
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-23 10:50:48 +0530

    Time: 227 ms (11.23%), Space: 15.4 MB (87.97%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,35 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        preReq = defaultdict(list)
+        courses = [0] * numCourses
+        
+        for course, req in prerequisites:
+            courses[course] += 1
+            if req not in preReq:
+                preReq[req] = []
+            preReq[req].append(course) 
+            
+        curCourses = []
+        for i in range(len(courses)):
+            if courses[i] == 0:
+                curCourses.append(i)
+        
+        coursesDone = 0
+        while curCourses:
+            nxtCourses = []
+            for course in curCourses:
+                coursesDone += 1
+                # remove pre req
+                for reqFor in preReq[course]:
+                    courses[reqFor] -= 1
+                    if courses[reqFor] == 0:
+                        nxtCourses.append(reqFor)
+                        
+            curCourses = nxtCourses
+        
+        return coursesDone == numCourses
+            
+                
+                
+        
