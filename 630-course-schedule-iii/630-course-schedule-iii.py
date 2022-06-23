57a1f40695202e23fbfb01709e3f3975a09364d2
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-23 10:04:01 +0530

    Time: 1346 ms (17.85%), Space: 20.1 MB (51.03%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.py
+++ 630-course-schedule-iii/630-course-schedule-iii.py
@@ -1,21 +1,12 @@
-# class Solution:
-#     def scheduleCourse(self, courses: List[List[int]]) -> int:
-#         courses.sort(key=lambda c: c[1])
-#         A, curr = [], 0
-#         for dur, ld in courses:
-#             heapq.heappush(A,-dur)
-#             curr += dur
-#             if curr > ld: curr += heapq.heappop(A)
-#         return len(A)
+
 
 class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
         heap = []
         
         courses.sort(key=lambda x:x[1])
-        # print(courses)
         curTotalTime = 0
-        maxSoFar = 0
+        
         for idx in range(len(courses)):
             duration, lastDay = courses[idx]
             
@@ -24,10 +15,6 @@ class Solution:
             
             if curTotalTime > lastDay:
                 curTotalTime +=  heapq.heappop(heap)
-            
 
-            # print(curTotalTime, heap)
-            maxSoFar = max(maxSoFar, len(heap))
-        
-        return maxSoFar
+        return len(heap)
         
