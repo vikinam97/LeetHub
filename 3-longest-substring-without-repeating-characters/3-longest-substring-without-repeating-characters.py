7510d2132956f6795b11e51e92f15c012f45854f
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-25 16:55:03 +0530

    Time: 108 ms (44.50%), Space: 14 MB (49.06%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,22 +1,39 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        seenMap = {}
         maxSoFar = 0
-        charSeen = defaultdict(int)
-        charCount = 0
-        j = 0 
+        j = 0
         for i in range(len(s)):
-            if charSeen[s[i]] == 0:
-                charCount += 1
-            charSeen[s[i]] += 1
-            
-            while charCount < (i - j + 1):
-                charSeen[s[j]] -= 1
-                if charSeen[s[j]] == 0:
-                    charCount -= 1
-                j += 1
-            
-            if charCount == (i - j + 1):
-                maxSoFar = max(maxSoFar, i - j + 1)
+            if s[i] in seenMap:
+                j = max(j, seenMap[s[i]])
+            seenMap[s[i]] = i + 1
+            maxSoFar = max(maxSoFar, i - j + 1)
         
         return maxSoFar
+                
+        
+        
+        
+        # Solution - sliding window
+        # Time - O(N)
+        # Space - O(N)
+#         maxSoFar = 0
+#         charSeen = defaultdict(int)
+#         charCount = 0
+#         j = 0 
+#         for i in range(len(s)):
+#             if charSeen[s[i]] == 0:
+#                 charCount += 1
+#             charSeen[s[i]] += 1
+            
+#             while charCount < (i - j + 1):
+#                 charSeen[s[j]] -= 1
+#                 if charSeen[s[j]] == 0:
+#                     charCount -= 1
+#                 j += 1
+            
+#             if charCount == (i - j + 1):
+#                 maxSoFar = max(maxSoFar, i - j + 1)
+        
+#         return maxSoFar
             
