2fb30060fb2eaf1628e28a1a5114f61f33c27ea0
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-25 10:30:13 +0530

    Time: 439 ms (5.19%), Space: 15.1 MB (91.17%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.py
+++ 665-non-decreasing-array/665-non-decreasing-array.py
@@ -7,13 +7,13 @@ class Solution:
                 if i == 1:
                     count += 1
                 else:
-                    j = i-2
-                    while j >= 0:
-                        if nums[j] > nums[i] and i < len(nums) - 1 and nums[j+1] > nums[i+1]:
-                            return False
-                        j -= 1
-                    # if nums[i-2] > nums[i] and i < len(nums) - 1 and nums[i-1] > nums[i+1]:
-                    #     return False
+                    # j = i-2
+                    # while j >= 0:
+                    #     if nums[j] > nums[i] and i < len(nums) - 1 and nums[j+1] > nums[i+1]:
+                    #         return False
+                    #     j -= 1
+                    if nums[i-2] > nums[i] and i < len(nums) - 1 and nums[i-1] > nums[i+1]:
+                        return False
                     count += 1
             
             if count > 1:
