52c5e3a2194484ef0daa4416952943a6769189ba
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-25 16:43:52 +0530

    Time: 111 ms (42.07%), Space: 14 MB (49.06%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,40 +1,22 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        last_seen = {}
-        l=0
-        max_length=0
-        for r in range(len(s)):
-            if s[r] in last_seen:
-                l=max(last_seen[s[r]], l)
+        maxSoFar = 0
+        charSeen = defaultdict(int)
+        charCount = 0
+        j = 0 
+        for i in range(len(s)):
+            if charSeen[s[i]] == 0:
+                charCount += 1
+            charSeen[s[i]] += 1
             
-            last_seen[s[r]]=r+1
-            max_length=max(max_length, r-l+1)
-        return max_length
+            while charCount < (i - j + 1):
+                charSeen[s[j]] -= 1
+                if charSeen[s[j]] == 0:
+                    charCount -= 1
+                j += 1
+            
+            if charCount == (i - j + 1):
+                maxSoFar = max(maxSoFar, i - j + 1)
         
-        
-        
-        # Solution
-        # advantage of contigious sub string sliding window
-        # Time O(n) traverse array ones
-        # Space O(n) max we store all char in set
-        # hashSet = {}
-        # charCount = 0
-        # maxSoFar = 0
-        # i, j = 0, 0
-        # while j < len(s):
-        #     # print(i, j, charCount, j - i + 1, hashSet)
-        #     # to check if it already in sub string
-        #     if hashSet.get(s[j], 0) == 0:
-        #         hashSet[s[j]] = 1
-        #         charCount += 1
-        #     # already in sub string thn remove from begging
-        #     else:
-        #         hashSet[s[j]] += 1
-        #         while charCount != (j - i + 1):
-        #             hashSet[s[i]] -= 1
-        #             if hashSet[s[i]] == 0:
-        #                 charCount -= 1
-        #             i += 1
-        #     maxSoFar = max(maxSoFar, j - i + 1)
-        #     j += 1
-        # return maxSoFar
+        return maxSoFar
+            
