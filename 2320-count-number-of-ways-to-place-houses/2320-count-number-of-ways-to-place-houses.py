08c6176b24e32a9091ee48c86241f6e569c6652b
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-27 19:45:22 +0530

    Time: 185 ms (80.00%), Space: 13.9 MB (100.00%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.py
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.py
@@ -1,22 +1,22 @@
 class Solution:
     def countHousePlacements(self, n: int) -> int:
-        def fib(x):
-            a, b, c = 1, 1, 0
+#         def fib(x):
+#             a, b, c = 1, 1, 0
             
-            for _ in range(x):
-                c = a + b
-                b, a = c, b
+#             for _ in range(x):
+#                 c = a + b
+#                 b, a = c, b
             
-            return c
-        # a, b = 1, 1
-        # ans = 0
-        # for i in range(n):
-        #     ans = a + b
-        #     a = b
-        #     b = ans
+#             return c
+        a, b = 1, 1
+        ans = 0
+        for i in range(n):
+            ans = a + b
+            a = b
+            b = ans
         
-        return (fib(n) ** 2) % (10 ** 9 + 7)
     
+        return (ans ** 2) % (10 ** 9 + 7)
 # class Solution:
 #     def countHousePlacements(self, n: int) -> int:
 #         def fib(x):
