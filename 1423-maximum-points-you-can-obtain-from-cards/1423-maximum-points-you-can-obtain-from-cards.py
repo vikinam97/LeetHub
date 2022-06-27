141943135c46c21e1df8e99892ac3ab436d66420
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-27 20:00:58 +0530

    Time: 760 ms (19.09%), Space: 26.9 MB (95.23%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,11 +1,13 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
+        # Solution - min sum sub array 
+        # Time - O(N)
+        # Space - O(1)
         length = len(cardPoints)
         slidingSum = sum(cardPoints[:length - k])
         minSoFar = slidingSum
         j = 0
         for i in range(length - k, length):
-            print(slidingSum)
             slidingSum += cardPoints[i]
             slidingSum -= cardPoints[j]
             j += 1
