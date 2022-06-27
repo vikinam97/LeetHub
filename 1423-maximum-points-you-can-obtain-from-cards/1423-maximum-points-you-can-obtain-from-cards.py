051524b7e442e52842cf16c589f3dd62a2cc9e86
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-27 19:59:43 +0530

    Time: 904 ms (6.02%), Space: 27.5 MB (17.51%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,16 +1,14 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        n = len(cardPoints)
-        total = sum(cardPoints)
+        length = len(cardPoints)
+        slidingSum = sum(cardPoints[:length - k])
+        minSoFar = slidingSum
+        j = 0
+        for i in range(length - k, length):
+            print(slidingSum)
+            slidingSum += cardPoints[i]
+            slidingSum -= cardPoints[j]
+            j += 1
+            minSoFar = min(minSoFar, slidingSum)
         
-        remaining_length = n - k
-        subarray_sum = sum(cardPoints[:remaining_length])
-        
-        min_sum = subarray_sum
-        for i in range(remaining_length, n):
-            # Update the sliding window sum to the subarray ending at index i
-            subarray_sum += cardPoints[i]
-            subarray_sum -= cardPoints[i - remaining_length]
-            # Update min_sum to track the overall minimum sum so far
-            min_sum = min(min_sum, subarray_sum)
-        return total - min_sum
+        return sum(cardPoints) -  minSoFar
