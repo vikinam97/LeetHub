781abd4d1d64c03a47462177509f7139a4f2916d
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-28 12:15:57 +0530

    Time: 249 ms (51.81%), Space: 15 MB (7.85%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -11,12 +11,9 @@ class Solution:
         deleteCount = 0
         seenFreq = {}
         for i in range(1,len(countingList)):
-            # if countingList[i] >= countingList[i-1]:
             while countingList[i] > 0 and countingList[i] >= countingList[i-1]:
                 countingList[i] -= 1
                 deleteCount += 1
-            print(countingList, countingList[i], countingList[i-1])
-        
         return deleteCount
         
         
