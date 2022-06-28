21a1bc28e423aacf873f3b18cff0993848bf2a12
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-06-28 13:50:57 +0530

    Time: 2247 ms (32.01%), Space: 28 MB (86.17%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.py
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.py
@@ -4,15 +4,16 @@ class Solution:
         deletedCount = 0
         for i in range(len(nums)-1):
             idx = i - deletedCount
-            if (idx & 1 == 0) and nums[i] != nums[i+1]:
-                stack.append(nums[i])
-            elif (idx & 1 == 1):
-                stack.append(nums[i])
+            if ((idx & 1 == 0) and nums[i] != nums[i+1]) or (idx & 1 == 1):
+                # stack.append(nums[i])
+            # elif (idx & 1 == 1):
+                # stack.append(nums[i])
+                print('ki');
             else:
                 deletedCount += 1
-        stack.append(nums[-1])
+        # stack.append(nums[-1])
         
-        if len(stack) & 1 == 1:
+        if (len(nums) - deletedCount) & 1 == 1:
             deletedCount += 1
         
         return deletedCount
