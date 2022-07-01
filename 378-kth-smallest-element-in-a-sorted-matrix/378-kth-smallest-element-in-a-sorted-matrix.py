3bd3a0e207920e660731aff4adb4b6f683fa76b1
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-01 21:01:21 +0530

    Time: 270 ms (59.12%), Space: 18.8 MB (37.86%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,11 +1,47 @@
 class Solution:
+    
+    def getCountOfNumsLessEqual(self, matrix, val):
+        r = len(matrix) - 1
+        c = 0
+        
+        count = 0
+        while c < len(matrix[0]) and r >= 0:
+            if matrix[r][c] <= val:
+                count += r + 1
+                c += 1
+            else:
+                r -= 1
+                
+        return count
+    
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        heap = []
         
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                heapq.heappush(heap, -1 * matrix[i][j])
-                if len(heap) > k:
-                    heapq.heappop(heap)
+        l = matrix[0][0]
+        r = matrix[len(matrix)-1][len(matrix[0]) - 1]
         
-        return -1 * heap[0]
+        pKval = -1
+        while l <= r:
+            mid = l + ((r - l) // 2)
+            
+            count = self.getCountOfNumsLessEqual(matrix, mid)
+            
+            if count >= k:
+                pKval = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        
+        return pKval
+                
+        
+        
+        # Solution - Max Heap
+        # Time - O(N^2)
+        # Space - O(K)
+        # heap = []
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix[i])):
+        #         heapq.heappush(heap, -1 * matrix[i][j])
+        #         if len(heap) > k:
+        #             heapq.heappop(heap)
+        # return -1 * heap[0]
