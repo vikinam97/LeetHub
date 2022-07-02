c368bc9442f0f81ba85084e4e17b9012c030bfdc
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-02 13:54:10 +0530

    Time: 254 ms (5.82%), Space: 18.6 MB (70.38%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,31 @@
+class HeapNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+    
+    def __lt__(self, other):
+        return self.val > other.val
+        
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        freq = defaultdict(int)
+        
+        for i in range(len(nums)):
+            if nums[i] not in freq:
+                node = HeapNode(nums[i], 1)
+                freq[nums[i]] = node
+                heap.append(node)
+            
+            freq[nums[i]].val += 1
+        
+        heapq.heapify(heap)
+        
+        result = []
+        while heap and len(result) < k:
+            result.append(heapq.heappop(heap).key)
+        
+        return result
+        
+        
+            
