0ba919d66c35fd9d78228371ba29b016287f4dc9
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-03 13:34:35 +0530

    Time: 2653 ms (28.17%), Space: 46.6 MB (5.24%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,15 +1,20 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
+        # Solution - division of array
+        # Time - O(N)
+        # Space - O(N)
         pos = []
         negs = []
-        for i in range(len(nums)):
+        i = len(nums) - 1
+        while i>=0:
             if nums[i] > 0:
                 pos.append(nums[i])
             else:
                 negs.append(nums[i])
+            i -= 1
         
-        pos.reverse()
-        negs.reverse()
+        # pos.reverse()
+        # negs.reverse()
         result = []
         while pos and negs:
             result.append(pos.pop())
