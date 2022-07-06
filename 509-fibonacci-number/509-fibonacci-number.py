8ea04cd5230c5878c072e71c25dacbc1ed2cdff9
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-06 11:44:42 +0530

    Time: 53 ms (48.24%), Space: 13.9 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,5 +1,17 @@
 class Solution:
     def fib(self, n: int) -> int:
+        ratio = (sqrt(5) + 1) / 2;
+        
+        uptopowers = 1;
+        while n > 0: 
+            if(n & 1):
+                uptopowers *= ratio;
+            ratio = ratio * ratio;
+            n = n >> 1; 
+        
+        return round(uptopowers / sqrt(5));
+    
+    def fib1(self, n: int) -> int:
         # Solution - Simple trversal
         # Time - O(N)
         # Space - O(1)
