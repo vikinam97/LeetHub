d120169628685ccc2cc60ca3f81dea7fb4b82177
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-06 14:07:13 +0530

    Time: 49 ms (41.48%), Space: 14 MB (12.60%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -3,18 +3,18 @@ class Solution:
         # Solution - Dynamic programming
         # Time - O(N ^ 2)
         # Space - O(N)
-#         dp = [0] * (n + 1)
-#         dp[0] = dp[1] = 1
+        dp = [0] * (n + 1)
+        dp[0] = dp[1] = 1
         
-#         for i in range(2, n + 1):
-#             dp[i] = 0
-#             for j in range(i):
-#                 dp[i] += dp[j] * dp[i-1-j]
+        for i in range(2, n + 1):
+            dp[i] = 0
+            for j in range(i):
+                dp[i] += dp[j] * dp[i-1-j]
         
-#         return dp[n]
-            
-        
-        
+        return dp[n]
+
+class Solution1:
+    def numTrees(self, n: int) -> int:
         # Solution recurrence with memorization
         # Time O(N ^2)
         # space O(N) memo and call stack
