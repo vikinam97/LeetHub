14c6af0b40771a43b19571445410f1cdca1a9240
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-08 15:28:36 +0530

    Time: 64 ms (17.17%), Space: 13.8 MB (90.37%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.py
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.py
@@ -5,6 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        # Solution - dfs
+        # Time - O(N)
+        # Space - O(N)
+        nodes = []
+        self.findNode(root, 0, None, x, y, nodes)
+        
+        if nodes[0][2] == nodes[1][2] and nodes[0][1] != nodes[1][1]:
+            return True
+        
+        return False
     def findNode(self, node, dep, parent, x, y, nodes):
         if not node:
             return;
@@ -17,12 +28,3 @@ class Solution:
             
         self.findNode(node.left, dep + 1, node, x, y, nodes)
         self.findNode(node.right, dep + 1, node, x, y, nodes)        
-        
-    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
-        nodes = []
-        self.findNode(root, 0, None, x, y, nodes)
-        
-        if nodes[0][2] == nodes[1][2] and nodes[0][1] != nodes[1][1]:
-            return True
-        
-        return False
