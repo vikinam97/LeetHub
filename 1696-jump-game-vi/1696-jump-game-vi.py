e0273f8c1467cb569e11e25384959e776ac55d1e
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-09 12:03:49 +0530

    Time: 2097 ms (17.62%), Space: 28.1 MB (47.64%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -22,24 +22,24 @@ class Solution:
         
         return q[-1][0]
                  
-class Solution1:
-    def maxResult(self, nums: List[int], k: int) -> int:        
-        # Solution  - Brute force checking on max sub sequence
-        # Time - O(N^2)
-        # Space - O(N)
-        if not nums:
-            return -1
+# class Solution1:
+#     def maxResult(self, nums: List[int], k: int) -> int:        
+#         # Solution  - Brute force checking on max sub sequence
+#         # Time - O(N^2)
+#         # Space - O(N)
+#         if not nums:
+#             return -1
         
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        for i in range(1, len(nums)):
-            maxInRange = float('-inf')
-            for j in range(max(0, i-k), i):
-                maxInRange = max(maxInRange, dp[j])
+#         dp = [0] * len(nums)
+#         dp[0] = nums[0]
+#         for i in range(1, len(nums)):
+#             maxInRange = float('-inf')
+#             for j in range(max(0, i-k), i):
+#                 maxInRange = max(maxInRange, dp[j])
             
-            dp[i] += nums[i] + maxInRange
+#             dp[i] += nums[i] + maxInRange
         
-        return dp[-1]
+#         return dp[-1]
             
         
         
