07282cbc2d7f101d26d76dbf49e5e26b08a6649b
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-10 11:10:55 +0530

    Time: 127 ms (10.21%), Space: 14 MB (75.93%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -2,13 +2,30 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         # Solution - dynamic programmin
         # Time - O(N)
-        # Space - O(N)
-        # dp = [0] * len(cost)
-        a, b = cost[0], cost[1]
+        # Space - O(1)
+        secondLast, last = cost[0], cost[1]
         
         for i in range(2, len(cost)):
             val = cost[i]
-            c = min(a + val, b + val)
-            a, b = b, c
+            cur = min(secondLast + val, last + val)
+            secondLast, last = last, cur
+        
+        return min(secondLast, last)
+    
+class Solution1:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # Solution - dynamic programmin
+        # Time - O(N)
+        # Space - O(N)
+        dp = [0] * len(cost)
+        dp[0], dp[1] = cost[0], cost[1]
+        
+        for i in range(2, len(cost)):
+            val = cost[i]
+            dp[i] = min(dp[i-1] +val, dp[i-2] + val)
+        
+        return min(dp[-1], dp[-2])
         
         return min(a, b)
+        
+        
