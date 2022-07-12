6740b3ae8db77a82b08ad78c10037bf8272852b4
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-12 11:46:02 +0530

    Time: 3268 ms (45.22%), Space: 583.2 MB (5.04%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.py
@@ -0,0 +1,31 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        sm = sum(matchsticks)
+        
+        if sm < 4:
+            return False
+        if sm % 4 != 0:
+            return False
+        
+        k = sm // 4
+        memo = {}
+                
+        matchsticks.sort(reverse=True)
+        
+        @cache
+        def recur(cur, s1, s2, s3, s4):            
+            if cur >= len(matchsticks):
+                if s1 == k and s2 == k and s3 == k and s4 == k:
+                    return True
+                return False
+            
+            if s1 > k or s2 > k or s3 > k or s4 > k:
+                return False
+            
+            return (recur(cur+1, s1 + matchsticks[cur], s2, s3, s4) or 
+                    recur(cur+1, s1, s2 + matchsticks[cur], s3, s4) or 
+                    recur(cur+1, s1, s2, s3 + matchsticks[cur], s4) or 
+                    recur(cur+1, s1, s2, s3, s4 + matchsticks[cur]))
+        
+        return recur(0, 0,0,0,0)
+        
