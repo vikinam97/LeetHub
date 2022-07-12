171ba1dcdabbf819a18d10f2041da42ed6195ae4
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-12 11:53:38 +0530

    Time: 4379 ms (30.44%), Space: 583.2 MB (5.04%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.py
+++ 473-matchsticks-to-square/473-matchsticks-to-square.py
@@ -12,6 +12,7 @@ class Solution:
                 
         matchsticks.sort(reverse=True)
         
+        @cache
         def recur(cur, s1, s2, s3, s4):            
             if cur >= len(matchsticks):
                 if s1 == k and s2 == k and s3 == k and s4 == k:
@@ -21,25 +22,10 @@ class Solution:
             if s1 > k or s2 > k or s3 > k or s4 > k:
                 return False
             
-            if (cur+1, s1, s2, s3, s4 + matchsticks[cur]) in memo:
-                return memo[(cur+1, s1, s2, s3, s4 + matchsticks[cur])]  
-            
-            memo[(cur+1, s1 + matchsticks[cur], s2, s3, s4)] = recur(cur+1, s1 + matchsticks[cur], s2, s3, s4)
-            if memo[(cur+1, s1 + matchsticks[cur], s2, s3, s4)]:
-                return True
-            memo[(cur+1, s1, s2 + matchsticks[cur], s3, s4)] = recur(cur+1, s1, s2 + matchsticks[cur], s3, s4)
-            if memo[(cur+1, s1, s2 + matchsticks[cur], s3, s4)]: 
-                return True
-            memo[(cur+1, s1, s2, s3 + matchsticks[cur], s4)] = recur(cur+1, s1, s2, s3 + matchsticks[cur], s4)
-            if memo[(cur+1, s1, s2, s3 + matchsticks[cur], s4)]:
-                return True
-            memo[(cur+1, s1, s2, s3, s4 + matchsticks[cur])] = recur(cur+1, s1, s2, s3, s4 + matchsticks[cur])
-            if memo[(cur+1, s1, s2, s3, s4 + matchsticks[cur])]: 
-                return True
-            # return (recur(cur+1, s1 + matchsticks[cur], s2, s3, s4) or 
-            #         recur(cur+1, s1, s2 + matchsticks[cur], s3, s4) or 
-            #         recur(cur+1, s1, s2, s3 + matchsticks[cur], s4) or 
-            #         recur(cur+1, s1, s2, s3, s4 + matchsticks[cur]))
+            return (recur(cur+1, s1 + matchsticks[cur], s2, s3, s4) or 
+                    recur(cur+1, s1, s2 + matchsticks[cur], s3, s4) or 
+                    recur(cur+1, s1, s2, s3 + matchsticks[cur], s4) or 
+                    recur(cur+1, s1, s2, s3, s4 + matchsticks[cur]))
         
         return recur(0, 0,0,0,0)
         
