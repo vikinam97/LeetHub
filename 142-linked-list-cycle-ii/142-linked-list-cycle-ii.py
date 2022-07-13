7248d9fbf63b01182a30327f15a304ca4369cb43
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-13 11:00:54 +0530

    Time: 80 ms (49.80%), Space: 17.5 MB (31.53%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -3,9 +3,32 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # first check cycle or not
+        slow, fast = head, head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        
+        if not fast or not fast.next:
+            return None
+        
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        
+        return slow
+    
+class Solution1:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Solution - Hash Map
+        # Time - O(N)
+        # Space - O(N)
         hashMap = {}
         while head:
             if head in hashMap:
