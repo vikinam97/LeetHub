d277276df602ff40bfe0abb1f3ece987aff6a085
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-14 13:47:30 +0530

    Time: 55 ms (68.74%), Space: 14.3 MB (56.22%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,65 +1,30 @@
 class Solution:
+    # Solution - binary srearch
+    # Time - O(log (N))
+    # Space - O(1)
     def search(self, nums: List[int], target: int) -> int:
-        return self.binary_search(nums, 0, len(nums)-1, target)
-    
-    
-    
-    def binary_search(self, nums, start, end, target):
-        if start > end:
-            return -1
+        l, r = 0, len(nums) -1
         
-        mid = (start+end)//2
-        
-        if nums[mid] == target:
-            return mid
-        
-        
-        # start to mid is sorted
-        if nums[start] <= nums[mid]:
-            if nums[start]<=target<nums[mid]:
-                return self.binary_search(nums, start, mid-1, target)
-            else:
-                return self.binary_search(nums, mid+1, end, target)
-        # mid to end is sorted
-        else:
-            if nums[mid]<target<=nums[end]:
-                return self.binary_search(nums, mid+1, end, target)
-            else:
-                return self.binary_search(nums, start, mid-1, target)
+        while l <= r:
+            mid = l + ((r - l) // 2)
             
-#     def search(self, nums: List[int], target: int) -> int:
-#         # step1 find the break point using binary search
-#         l, r = 0, len(nums) - 1
-#         breakpoint = len(nums) - 1
-#         while l <= r:
-#             mid = (r + l) // 2
-#             if (mid < r and nums[mid + 1] < nums[mid]) or (mid > 0 and nums[mid - 1] > nums[mid]):
-#                 breakpoint = mid
-#                 break
-#             elif nums[mid] > nums[l]:
-#                 l = mid + 1
-#             else:
-#                 r = mid - 1
-#         print(breakpoint)
-        
-#         # find the nums in segement
-#         if nums[0] < target < nums[breakpoint]:
-#             l, r = 0, breakpoint 
-#         else: 
-#             l, r = breakpoint + 1, len(nums) - 1
-        
-#         print(nums[l:r+1])
-#         while l <= r:
-#             mid = (r + l) // 2
-#             # print(l, r, mid)
-#             if nums[mid] == target:
-#                 return mid
-#             elif nums[mid] < target:
-#                 l = mid + 1 
-#             else:
-#                 r = mid - 1
-        
-#         return -1
+            if nums[mid] == target:
+                return mid
+            
+            # left arr
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+                    
+        return -1
+                
         
         
             
