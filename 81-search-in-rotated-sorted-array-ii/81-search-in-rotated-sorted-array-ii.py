9811f7a4ba152caa4f3b147bbd111c1ab79a0a1d
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-14 13:58:11 +0530

    Time: 101 ms (21.57%), Space: 14.5 MB (59.12%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -5,6 +5,7 @@ class Solution:
         
         # for l == r on [1] arr
         while l <= r:
+            # skipping left =`s and right =`s
             while l < r and nums[l] == nums[l+1]:
                 l += 1
             while r > l and nums[r] == nums[r-1]:
@@ -16,12 +17,12 @@ class Solution:
                 return True
             
             if nums[l] <= nums[mid]:
-                if nums[l] <= target < nums[mid]:
+                if nums[l] <= target <= nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
             else:
-                if nums[mid] < target <= nums[r]:
+                if nums[mid] <= target <= nums[r]:
                     l = mid + 1
                 else:
                     r = mid - 1
