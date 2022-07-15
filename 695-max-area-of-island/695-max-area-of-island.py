7c4fc30f6a721a5eabed455e05b6110d16aca750
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-15 10:58:51 +0530

    Time: 217 ms (51.20%), Space: 18.8 MB (5.15%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,6 +1,21 @@
 class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        # Solution - DFS
+        # Time - O(R*C)
+        # Space - O(R*C)
+        
+        longSoFar = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                count = self.dfs(i, j, grid)
+                longSoFar = max(longSoFar, count)
+
+        return longSoFar
+    
     def dfs(self, x, y, grid):
-        if not (0 <= x < len(grid)) or not( 0 <= y < len(grid[0])) or grid[x][y] != 1:
+        if x >= len(grid) or x < 0 or y >= len(grid[0]) or y < 0 or grid[x][y] != 1:
+        # if not (0 <= x < len(grid)) or not( 0 <= y < len(grid[0])) or grid[x][y] != 1:
             return 0
         
         di = [[0, 1], [0, -1], [1, 0], [-1, 0]]
@@ -11,14 +26,3 @@ class Solution:
             sm += self.dfs(x + xi, y + yi, grid)
         
         return sm
-        
-        
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        longSoFar = 0
-        
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                count = self.dfs(i, j, grid)
-                longSoFar = max(longSoFar, count)
-
-        return longSoFar
