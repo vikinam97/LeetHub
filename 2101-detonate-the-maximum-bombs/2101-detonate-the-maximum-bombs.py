667cf26d838d13caf6fb84159f9fe373dcbfa5d9
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-15 17:06:56 +0530

    Time: 5890 ms (8.38%), Space: 13.8 MB (99.83%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -0,0 +1,31 @@
+class Solution:
+    def bfs(self, cur, bombs):
+        bfsList = [(bombs[cur], cur)]
+        detonatedMap = { cur: True }
+        
+        count = 0
+        while bfsList:
+            nxt = []
+            for bomb, idx in bfsList:
+                x, y, r = bomb
+                count += 1
+                for id2 in range(len(bombs)):
+                    xi, yi, ri = bombs[id2]
+                    if id2 not in detonatedMap and math.dist([x, y], [xi, yi]) <= r:
+                        nxt.append((bombs[id2], id2))
+                        detonatedMap[id2] = True
+            bfsList = nxt
+        return count
+        
+        
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        maxSoFar, maxi = 0, -1
+        for i in range(len(bombs)):
+            tmax = self.bfs(i, bombs)
+            if maxSoFar < tmax:
+                maxSoFar, maxi = tmax, i
+        
+        return maxSoFar
+        
+            
+            
