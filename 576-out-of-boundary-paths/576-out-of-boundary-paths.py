67bb539764a1c8f10f46d8f49ae7da638c8db7dc
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-16 10:57:40 +0530

    Time: 137 ms (81.33%), Space: 24.6 MB (5.37%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.py
@@ -1,12 +1,20 @@
 class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        # Solution - recursion with memo
+        # Time - O(M * R * C)
+        # Space - O(M * R * C)
+        count = self.noOfWays(startRow, startColumn, maxMove, m, n)
+            
+        return count % ((10 ** 9) + 7)
     
     @cache
     def noOfWays(self, x, y, moves, m, n):
-        if x < 0 or x >= m or y < 0 or y >= n:
+        out = x < 0 or x >= m or y < 0 or y >= n
+        if out:
             return 1
         
         if moves <= 0:
-            if x < 0 or x >= m or y < 0 or y >= n:
+            if out:
                 return 1
             return 0
         
@@ -16,8 +24,3 @@ class Solution:
             count += self.noOfWays(x + xi, y + yi, moves - 1, m, n)
         
         return count
-    
-    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
-        count = self.noOfWays(startRow, startColumn, maxMove, m, n)
-            
-        return count % ((10 ** 9) + 7)
