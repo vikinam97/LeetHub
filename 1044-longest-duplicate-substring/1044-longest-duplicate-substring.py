80f4453e85ab5d6d535661163ed3d7ea1117ce81
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-20 17:23:57 +0530

    Time: 2972 ms (64.20%), Space: 18.1 MB (87.20%) - LeetHub

--- 1044-longest-duplicate-substring/1044-longest-duplicate-substring.py
+++ 1044-longest-duplicate-substring/1044-longest-duplicate-substring.py
@@ -3,18 +3,26 @@ class Solution:
         chrMap = [ord(c) - ord('a') for c in s]
         mod = 2**63 - 1 
         
-        def checkSubstr(L):
-            cur, k = 0, 1
-            for i in range(L):
-                cur = (cur + chrMap[L-1-i]*k)%mod
-                k = k*26 % mod
-            seen = {cur}
-            for i in range(L, len(s)):
-                cur = (cur * 26 + chrMap[i] - chrMap[i - L] * k) % mod
-                if cur in seen: return i - L + 1
-                seen.add(cur)
-            return None
+        def checkSubstr(ln):
+            roll, k = 0, 1
+            seenMap = {}
+
+            for i in range(ln):
+                roll = (roll + chrMap[ln -1 - i] * k) % mod
+                k = (k * 26) % mod
+            seenMap[roll] = True
             
+            for i in range(ln, len(s)):
+                delTerm = chrMap[i - ln] * k
+                addTerm = chrMap[i]
+                roll = (roll * 26 + addTerm - delTerm) % mod
+                
+                if roll in seenMap:
+                    return i - ln + 1
+                
+                seenMap[roll] = True
+            
+            return None
         l, r = 0, len(s) - 1
         x, y = 0, 0
         while l <= r:
