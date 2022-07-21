f4f59d54711f940880f14b9f11be7bf779247245
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-21 19:08:39 +0530

    Time: 2124 ms (5.83%), Space: 138.8 MB (18.07%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,14 +1,19 @@
 class Solution:
-    @cache
     def recur(self, i, j, text1, text2):
         if i >= len(text1) or j >= len(text2):
             return 0
         
-        if text1[i] == text2[j]:
-            return self.recur(i+1, j+1, text1, text2) + 1
+        if (i, j) in self.memo:
+            return self.memo[(i, j)]
         
-        return max(self.recur(i, j+1, text1, text2),
+        if text1[i] == text2[j]:
+            self.memo[(i, j)] = self.recur(i+1, j+1, text1, text2) + 1
+        else:
+            self.memo[(i, j)] = max(self.recur(i, j+1, text1, text2),
                   self.recur(i+1, j, text1, text2))
+        
+        return self.memo[(i, j)]
     
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        self.memo = {}
         return self.recur(0, 0, text1, text2)
