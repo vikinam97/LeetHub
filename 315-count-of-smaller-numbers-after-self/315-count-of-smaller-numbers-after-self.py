d6ab162db94e708abaa57b3ab6c4889d7d5f50d1
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-23 12:25:18 +0530

    Time: 5295 ms (28.87%), Space: 41.7 MB (23.42%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -1,12 +1,48 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
         
-        #a = SortedList()
-        a = []
-        ans = []
+        n = len(nums)
+        arr = [[v, i] for i, v in enumerate(nums)]
         
-        for v in nums[::-1]:
-            ans.append(bisect.bisect_left(a, v))
-            bisect.insort(a, v)
+        result = [0]*n
         
-        return ans[::-1]
+        def merge_sort(arr, left, right):
+            if left >= right:
+                return 
+            mid = left + (right-left)//2
+            merge_sort(arr, left, mid)
+            merge_sort(arr, mid+1, right)
+            merge(arr, left, mid, mid+1, right)
+            
+        
+        def merge(arr, ls, le, hs, he):
+            i = ls
+            j = hs
+            temp = []
+            rightcount = 0
+            
+            while i <= le and j <= he:
+                if arr[j][0] < arr[i][0]:
+                    rightcount += 1
+                    temp.append(arr[j])
+                    j += 1
+                else:
+                    result[arr[i][1]] += rightcount
+                    temp.append(arr[i])
+                    i += 1
+            
+            while i <= le:
+                result[arr[i][1]] += rightcount
+                temp.append(arr[i])
+                i += 1
+            
+            while j <= he:
+                temp.append(arr[j])
+                j += 1
+            
+            
+            arr[ls:he+1] = temp
+            
+        
+        merge_sort(arr, 0, n-1)
+        return result
