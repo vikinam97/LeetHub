b28d22a931fe1329c612fb7fc1de2294f0f5e80a
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-25 11:19:36 +0530

    Time: 276 ms (24.36%), Space: 14 MB (24.57%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -4,13 +4,6 @@ class Node:
         self.next = next
         
 class Solution:
-    def printList(self, list):
-        seen = {}
-        while list.val not in seen:
-            print(list.val)
-            seen[list.val] = True
-            list = list.next
-            
     def findTheWinner(self, n: int, k: int) -> int:
         head = Node("/")
         temp = head
@@ -25,15 +18,13 @@ class Solution:
         
         count = 0
         while count < n:
-            self.printList(temp)
             for i in range(1, k):
                 prev = temp
                 temp = temp.next
             prev.next = temp.next
-            print("left -> ", temp.val)
             temp = prev.next
             count += 1
-        print(count)
+
         return temp.val
         
         
