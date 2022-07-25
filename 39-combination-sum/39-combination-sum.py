aa145f78903b47528162aaf1642443c2b714551b
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-25 19:59:43 +0530

    Time: 171 ms (25.70%), Space: 14 MB (81.96%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,4 +1,16 @@
 class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # Solution - backtrack
+        # Time - O(N^M)
+        #     - N = no of cands
+        #     - M = target (worst case cands 1 so max M call stack)
+        # Space - O(N^M)
+        
+        self.comboResult = []
+        candidates.sort()
+        self.backtrack(0, [], candidates, target)
+        return self.comboResult
+    
     def backtrack(self, pathSum, path, cand, tar):
         if pathSum > tar:
             return
@@ -11,24 +23,4 @@ class Solution:
             path.append(num)
             self.backtrack(pathSum + num, path, cand[idx:], tar)
             path.pop()
-    
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        self.comboResult = []
-        candidates.sort()
-        self.backtrack(0, [], candidates, target)
-        return self.comboResult
-
-# class Solution(object):
-#     def combinationSum(self, candidates, target):
-#         ret = []
-#         self.dfs(candidates, target, [], ret)
-#         return ret
-    
-#     def dfs(self, nums, target, path, ret):
-#         if target < 0:
-#             return 
-#         if target == 0:
-#             ret.append(path)
-#             return 
-#         for i in range(len(nums)):
-#             self.dfs(nums[i:], target-nums[i], path+[nums[i]], ret)
+        
