9af6512b8892038dcd81d43ffc630cce37b7e4fd
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-26 13:23:12 +0530

    Time: 8675 ms (26.53%), Space: 13.9 MB (50.90%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,34 @@
+class Solution:
+    def backtrack(self, i, j, wi, board, visited, word):
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or visited[i][j] != 0:
+            return wi >= len(word) 
+        
+        if wi >= len(word):
+            return True
+        
+        if board[i][j] != word[wi]:
+            return False
+        
+        visited[i][j] = 1
+        for di, dj in self.diList:
+            x, y = i + di, j + dj
+            
+            if self.backtrack(x, y, wi + 1, board, visited, word):
+                return True
+        
+        visited[i][j] = 0
+        return False
+                
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        self.diList = [[1, 0], [-1, 0], [0, -1], [0, 1]]
+        visited = [[0] * len(board[0]) for i in range(len(board))]
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.backtrack(i, j, 0, board, visited, word):
+                    return True
+        
+        # self.backtrack(1, 3, 0, board, visited, word)
+        
+        return False
+        
