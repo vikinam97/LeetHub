a81ed205a4dc5c36fffe5973ed0023e8b713eaf7
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-26 19:38:13 +0530

    Time: 34 ms (92.65%), Space: 14 MB (22.22%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        hashMap = defaultdict(int)
+        for i in range(len(s)):
+            hashMap[s[i]] += 1
+            
+        totalCount, oddCount = 0, 0
+        for key in hashMap:
+            count = hashMap[key]
+            if count & 1:
+                totalCount += (count - 1)
+                oddCount += 1
+            else:
+                totalCount += count
+        
+        return totalCount if oddCount == 0 else totalCount + 1 
+                
+        
+        
