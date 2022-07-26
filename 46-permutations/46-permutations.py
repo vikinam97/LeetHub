cbe7217f3f8f1b13195a3a42491860520742f9b2
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-26 12:15:28 +0530

    Time: 41 ms (93.47%), Space: 14.1 MB (57.64%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,19 +1,23 @@
 class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # Solution - all combo
+        # Time - O(N!)
+        # Space - O(N!)
+        
+        self.perList = []
+        self.recur(nums, [], [0] * len(nums))
+        return self.perList
+    
     def recur(self, nums, path, used):
         if len(path) == len(nums):
             self.perList.append(path[:])
             return
         
-        for num in nums:
-            if num not in used:
-                used.add(num)
+        for idx, num in enumerate(nums):
+            if used[idx] == 0:
+                used[idx] = 1
                 path.append(num)
                 self.recur(nums, path, used)
                 path.pop()
-                used.remove(num)
-    
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        self.perList = []
-        self.recur(nums, [], set())
-        return self.perList
+                used[idx] = 0
         
