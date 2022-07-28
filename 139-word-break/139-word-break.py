3d836355639063f3ec984d8e11e15a9ef9a38a1c
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-28 19:48:17 +0530

    Time: 41 ms (90.87%), Space: 14 MB (71.00%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,17 +1,13 @@
 class Solution:
-    def __init__(self):
-        self.memo = dict()
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        if s in self.memo:
-            return self.memo[s]
-        else:
-            if s in wordDict:
-                return True
-            self.memo[s]=False
-            for i in range(1,len(s)):
-                if (self.wordBreak(s[:i],wordDict) and self.wordBreak(s[i:],wordDict)):
-                    self.memo[s]=True
-            return self.memo[s]
+        hashtable, dp = {i for i in wordDict}, [False] * (len(s) + 1)
+        dp[0] = True
+        for i in range(1, len(s) + 1):
+            for j in range(0, i):
+                if dp[j] and s[j:i] in hashtable:
+                    dp[i] = True
+                    break
+        return dp[len(s)]
 
 class Solution1:
     
