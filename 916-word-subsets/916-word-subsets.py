b805dfa198b9b2b7e21cff81f48a696efb204654
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-30 09:55:00 +0530

    Time: 742 ms (91.67%), Space: 18.5 MB (73.00%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.py
@@ -0,0 +1,30 @@
+class Solution:
+    
+    def isUniversal(self, word, rHash):
+        wHash = [0] * 26
+        for char in word:
+            wHash[ord(char) - ord('a')] += 1
+            
+        for i in range(26):
+            if rHash[i] > wHash[i]:
+                return False
+        
+        return True
+        
+    
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        reqHash = [0] * 26
+        for word in words2:
+            wHash = [0] * 26
+            for char in word:
+                key = ord(char) - ord('a')
+                wHash[key] += 1
+                reqHash[key] = max(wHash[key], reqHash[key])
+        result = []
+        
+        for word in words1:
+            if self.isUniversal(word, reqHash):
+                result.append(word)
+        
+        return result
+        
