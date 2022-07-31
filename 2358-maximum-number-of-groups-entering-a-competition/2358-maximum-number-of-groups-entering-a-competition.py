c3deae5669ecc3bb6f5ba70ac8f2be89ca7daee2
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-07-31 10:02:00 +0530

    Time: 940 ms (16.67%), Space: 27.3 MB (16.67%) - LeetHub

---
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximumGroups(self, grades: List[int]) -> int:
+        grades.sort()
+        
+        curSum, curTotal = 0, 0
+        lSum, lTotal = 0, 0
+        count = 0
+        i = 0
+        
+        for i in range(len(grades)):
+            curSum += grades[i]
+            curTotal += 1
+            
+            if curSum > lSum and curTotal > lTotal:
+                count += 1
+                lSum, lTotal = curSum, curTotal
+                curSum, curTotal = 0, 0
+        
+        return count
+            
+        
