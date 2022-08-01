3a391cf3baebd44ce9f8fac7a10fb6e330f04dee
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-01 10:25:30 +0530

    Time: 40 ms (76.12%), Space: 13.9 MB (73.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,4 +1,30 @@
 class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # Solution - dynamic programming | memoization
+        # Time - O(M * N)
+        # Space - O(M * N)
+        
+        dp = [[0]*n for i in range(m)]
+        
+        for i in range(m):
+            dp[i][0] = 1
+        for j in range(n):
+            dp[0][j] = 1
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return dp[-1][-1]
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # Solution - dynamic programming | memoization
+        # Time - O(M * N)
+        # Space - O(M * N)
+        
+        self.memo = {}
+        return self.recur(0, 0, m, n)
     
     def recur(self, i, j, m, n):
         if i > m or j > n:
@@ -13,7 +39,5 @@ class Solution:
         self.memo[(i, j)] = self.recur(i, j+1, m, n) + self.recur(i+1, j, m, n)
         return self.memo[(i, j)]
     
-    def uniquePaths(self, m: int, n: int) -> int:
-        self.memo = {}
-        return self.recur(0, 0, m, n)
+
         
