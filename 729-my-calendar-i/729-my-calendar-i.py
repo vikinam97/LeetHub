c9c0a4201cb9b38da0f7ea02a408143311010248
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-03 11:30:50 +0530

    Time: 374 ms (65.97%), Space: 14.8 MB (29.38%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -1,4 +1,19 @@
-class Node:
+class MyCalendar:
+    # Solution - using BST of intervals
+    # Time - O(NlogN) worst O(N^2) 
+    # Space - O(N)
+    
+    def __init__(self):
+        self.bookings = None
+
+    def book(self, start: int, end: int) -> bool:
+        if self.bookings == None:
+            self.bookings = BSTNode(start, end)
+            return True
+        
+        return self.bookings.checkInsert(start, end)
+
+class BSTNode:
     def __init__(self, start, end):
         self.start = start
         self.end = end
@@ -6,7 +21,6 @@ class Node:
         self.right = None
         
     def checkInsert(self, start, end):
-        # print(self.start, self.end)
         if not self:
             return 
         
@@ -17,33 +31,12 @@ class Node:
             if self.right:
                 return self.right.checkInsert(start, end)
             
-            self.right = Node(start, end)
+            self.right = BSTNode(start, end)
             return True
         
         elif end <= self.start:
             if self.left:
                 return self.left.checkInsert(start, end)
             
-            self.left = Node(start, end)
+            self.left = BSTNode(start, end)
             return True
-            
-
-class MyCalendar:
-    def __init__(self):
-        self.bookings = None
-
-    def book(self, start: int, end: int) -> bool:
-        # print("-----------")
-        # print(start, end)
-        # print("-")
-        if self.bookings == None:
-            self.bookings = Node(start, end)
-            return True
-        
-        return self.bookings.checkInsert(start, end)
-        
-
-
-# Your MyCalendar object will be instantiated and called as such:
-# obj = MyCalendar()
-# param_1 = obj.book(start,end)
