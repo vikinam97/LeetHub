de8b1847fda2b2b1e563fafff7e0c2176ed9b925
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-05 08:41:20 +0530

    Time: 42 ms (92.04%), Space: 13.9 MB (80.97%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,14 +1,19 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
+        # Solution - DP (coin change)
+        # Time - O(N)
+        #     - N = target
+        # Space - O(N)
+        
         dp = [0]*(target+1)
         dp[0] = 1
         
+        nums.sort()
+        
         for tar in range(target+1):
-            
             for num in nums:
                 if num > tar:
-                    continue
-                
+                    break
                 dp[tar] += dp[tar-num]
         
         return dp[-1]
