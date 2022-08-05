e13c84af28d4aff70530a878f8518047edd77ab3
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-05 19:48:51 +0530

    Time: 280 ms (66.15%), Space: 15.5 MB (40.20%) - LeetHub

--- 959-regions-cut-by-slashes/959-regions-cut-by-slashes.py
+++ 959-regions-cut-by-slashes/959-regions-cut-by-slashes.py
@@ -1,31 +1,44 @@
 class Solution:
+    
+    def dfs(self, i, j, k, grid, height, width):
+        if i < 0 or i >= height or j < 0 or j >= width:
+            return
+
+        if self.visited[i][j][k] != 0:
+            return
+        
+        self.visited[i][j][k] = 1
+        
+        if k == 0:
+            self.dfs(i-1, j, 2, grid, height, width)
+        elif k == 1:
+            self.dfs(i, j+1, 3, grid, height, width)
+        elif k == 2:
+            self.dfs(i+1, j, 0, grid, height, width)
+        elif k == 3:
+            self.dfs(i, j-1, 1, grid, height, width)
+            
+        if grid[i][j] != '/':
+            self.dfs(i, j, k ^ 1, grid, height, width)
+        if grid[i][j] != '\\':
+            self.dfs(i, j, k ^ 3, grid, height, width)
+    
     def regionsBySlashes(self, grid):
-        n, dict1 = len(grid), {}
-
-        def find(x):
-            if x not in dict1:
-                return x
-            else:
-                return find(dict1[x])
-
-        def union(x,y):
-            parent_x, parent_y = find(x), find(y)
-
-            if parent_x != parent_y:
-                dict1[parent_y] = parent_x
-
-
-        for i in range(n):
-            for j in range(n):
-                if i>0: union((i-1,j,2), (i,j,0))
-                if j>0: union((i,j-1,1), (i,j,3))
-
-                if grid[i][j] != "/":
-                    union((i,j,0), (i,j,1))
-                    union((i,j,2), (i,j,3))
-
-                if grid[i][j] != "\\":
-                    union((i,j,0), (i,j,3))
-                    union((i,j,1), (i,j,2))
-
-        return len({find(i) for i in dict1})
+        height, width = len(grid), len(grid[0])
+        
+        self.visited = [[[0]*4 for j in range(width)] for i in range(height)]
+        
+        count = 0
+        for i in range(height):
+            for j in range(width):
+                
+                for k in range(4):
+                    if self.visited[i][j][k] == 1:
+                        continue
+                        
+                    self.dfs(i, j, k, grid, height, width)
+                    count += 1
+        
+        return count
+        
+        
