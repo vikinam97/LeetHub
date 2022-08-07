d7af5705423a9874e15186626ead0f5562085b08
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-07 11:38:29 +0530

    Time: 1266 ms (24.48%), Space: 142.4 MB (23.57%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,6 +1,37 @@
 class Solution:
     
     MOD = (10 ** 9) + 7
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    adj = {
+        0: [1], 
+        1: [0, 2], 
+        2: [0, 1, 3, 4],
+        3: [2, 4], 
+        4: [0]
+    }
+
+    def countVowelPermutation(self, n: int) -> int:
+        
+        dp = [[0] * 5 for i in range(n)]
+        for i in range(5):
+            dp[0][i] = 1
+            
+        for i in range(1, n):
+            for j in range(5):
+                for ni in self.adj[j]:
+                    dp[i][j] = dp[i][j] + dp[i-1][ni]
+        
+        # print(dp[-1])
+        p = 0
+        for val in dp[-1]:
+            # print(val)
+            p = (p + val % self.MOD) % self.MOD
+
+        return p
+
+class Solution1:
+    
+    MOD = (10 ** 9) + 7
     @cache
     def recur(self, cur, n):
         if n == 0:
@@ -14,14 +45,10 @@ class Solution:
         return p
     
     def countVowelPermutation(self, n: int) -> int:
-        # adj = {
-        #     -1: ['a', 'e', 'i', 'o','u'],
-        #     'a': ['e'],
-        #     'e': ['a', 'i'],
-        #     'i': ['a', 'e', 'o', 'u'],
-        #     'o': ['i', 'u'],
-        #     'u': ['a']
-        # }
+        # Solution - DP
+        # Time - O(N*5)
+        # Space - O(N*5)
+        
         self.adjList = {(-1, 'a'): 1, (-1, 'e'): 1, (-1, 'i'): 1, (-1, 'o'): 1, (-1, 'u'): 1, ('a', 'e'): 1, ('e', 'a'): 1, ('e', 'i'): 1, ('i', 'a'): 1, ('i', 'e'): 1, ('i', 'o'): 1, ('i', 'u'): 1, ('o', 'i'): 1, ('o', 'u'): 1, ('u', 'a'): 1}
         
         return self.recur(-1, n)
