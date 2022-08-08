49cc2d8d57a37c88d760e0f04e285c991946dc66
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-08 12:35:26 +0530

    Time: 536 ms (5.60%), Space: 14.5 MB (37.73%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,22 +1,66 @@
+class DSUF:
+    def __init__(self, n):
+        self.arr = [-1] * n
+    
+    def find(self, node):
+        if self.arr[node] == -1:
+            return node
+        return self.find(self.arr[node])
+    
+    def union(self, a, b):
+        aP = self.find(a)
+        bP = self.find(b)
+        if aP == bP:
+            return 
+        self.arr[aP] = bP
+        
+    def countParent(self):
+        count = 0
+        for i in self.arr:
+            if i == -1:
+                count += 1
+        return count
+
 class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        V = len(isConnected)
+        dSet = DSUF(V)
+        
+        for i in range(V):
+            for j in range(V):
+                if isConnected[i][j] == 1:
+                    dSet.union(i, j)
+        
+        return dSet.countParent()
+                
+
     
     
-    def dfs(self, cur, adjList):
-        if self.visited[cur] != 0:
-            return
-        
-        self.visited[cur] = 1
-        
-        for node in adjList[cur]:
-            self.dfs(node, adjList)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+class Solution1:
     
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        # Solution - DFS
+        # Time - O(V+E)
+        # Space - O(V+E)
+        
         count = 0
         height, width = len(isConnected), len(isConnected[0])
         
         adjList = defaultdict(list)
-        
-        
         for i in range(height):
             for j in range(width):
                 if isConnected[i][j] == 1:
@@ -31,3 +75,12 @@ class Solution:
             count += 1
                     
         return count
+    
+    def dfs(self, cur, adjList):
+        if self.visited[cur] != 0:
+            return
+        
+        self.visited[cur] = 1
+        
+        for node in adjList[cur]:
+            self.dfs(node, adjList)
