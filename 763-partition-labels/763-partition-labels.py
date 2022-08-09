850f93e2fe6b91948cc040479dfc36cf47af610b
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-09 09:52:09 +0530

    Time: 63 ms (53.43%), Space: 13.7 MB (99.84%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.py
@@ -0,0 +1,29 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        lastSeen = defaultdict(int)
+        for i in range(len(s)):
+            lastSeen[s[i]] = i
+        
+        i, j = 0, 0
+        seenChar = set()
+        
+        result = []
+        
+        while i < len(s):
+            seenChar.add(s[i])
+            
+            possiblePartition = True
+            for char in seenChar:
+                if lastSeen[char] > i:
+                    possiblePartition = False
+            
+            i += 1
+            
+            if possiblePartition:
+                seenChar = set()
+                result.append(i - j)
+                j = i
+        
+        return result
+                
+            
