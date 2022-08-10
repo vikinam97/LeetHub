8224eb77bc02d0f37fa28bf9bffa61668e88f7e2
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-10 10:31:07 +0530

    Time: 144 ms (97.41%), Space: 15.6 MB (26.55%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -2,20 +2,22 @@ import collections
 
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
+        # Solution - Greedy choose the one with largest range for every stand
+        # Time - O(NlogN)
+        # Space - O(N)
+        
         rangeList = []
         for i in range(n+1):
             rangeList.append((i - ranges[i], i + ranges[i]))
         
         rangeList.sort()
         rangeList = collections.deque(rangeList)
-        # print(rangeList)
-        start = 0
-        count = 0
+        
+        start, count = 0, 0
+
         while start < n:
             end = start
-            # print("---", start)
             while rangeList and rangeList[0][0] <= start:
-                # print(rangeList[0])
                 end = max(end, rangeList.popleft()[1])
             
             if start == end:
