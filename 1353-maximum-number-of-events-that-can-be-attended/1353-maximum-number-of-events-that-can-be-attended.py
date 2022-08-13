25e00040fa4c2a125b541eb9164ea753f923cb0b
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-13 17:00:48 +0530

    Time: 2227 ms (22.23%), Space: 61.6 MB (22.54%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
@@ -1,21 +1,9 @@
-# class Solution:
-#     def maxEvents(self, events: List[List[int]]) -> int:
-#         days = [0] * ((10**5)+1)
-        
-#         events.sort(key = lambda x: (-1*x[0], x[1]))
-        
-#         count = 0
-        
-#         for s, e in events:
-#             for day in reversed(range(s, e+1)):
-#                 if days[day] == 0:
-#                     days[day] = 1
-#                     count += 1
-#                     break
-        
-#         return count
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
+        # Solution - attend event with recent end from current day
+        # Time - O(NlogN)
+        # Space - O(N)
+        
         events.sort()
         heap = []
         lastDay = max(events, key=lambda x: x[1])[1]
@@ -36,5 +24,31 @@ class Solution:
                 attended += 1
         
         return attended
+
+
+class Solution1:
+    # TLE - attend event on last possible date of event
+    # Time - O(NlogN) + O(N*(S-E))
+    #     - N - no of events
+    #     - E - end of event
+    #     - S - start of event
+    # Space - O(N)
+    
+    def maxEvents(self, events: List[List[int]]) -> int:
+        days = [0] * ((10**5)+1)
+        
+        events.sort(key = lambda x: (-1*x[0], x[1]))
+        
+        count = 0
+        
+        for s, e in events:
+            for day in reversed(range(s, e+1)):
+                if days[day] == 0:
+                    days[day] = 1
+                    count += 1
+                    break
+        
+        return count
+
             
             
