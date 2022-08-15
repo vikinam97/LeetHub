bf303fe101c71adc72a7be387f68a9af1d81594b
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-15 20:47:22 +0530

    Time: 130 ms (82.26%), Space: 14.4 MB (13.84%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,5 +1,53 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        arr = []
+        for i in range(len(nums)):
+            if not arr:
+                arr.append(nums[i])
+            else:
+                if arr[-1] < nums[i]:
+                    arr.append(nums[i])
+                else:
+                    idx = bisect.bisect_left(arr, nums[i])
+                    arr[idx] = nums[i]
+        
+        return len(arr)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
 class Solution1:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # Solution - LIS
+        # Time - O(N^2)
+        # Space - O(N)
+        
+        dp = [1] * len(nums)
+
+        maxSoFar = 1
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]: 
+                    dp[i] = max(dp[i],dp[j] + 1)
+            maxSoFar = max(dp[i], maxSoFar)
+
+        return maxSoFar
     
+class Solution1:
     def recur(self, i, prev, nums):
         if i >= len(nums):
             return 0
@@ -16,47 +64,8 @@ class Solution1:
         return result
     
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # Solution - recusion + Memoization
+        # Time - O(N^2)
+        # Space - O(N^2)
         self.memo = [[-1] * (len(nums) + 1) for _ in range(len(nums) + 1)]
         return self.recur(0, -1, nums)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        # Solution - LIS
-        # Time - O(N^2)
-        # Space - O(N)
-        
-        dp = [1] * len(nums)
-
-        maxSoFar = 1
-        for i in range(1, len(nums)):
-            for j in range(i):
-                if nums[j] < nums[i]: 
-                    dp[i] = max(dp[i],dp[j] + 1)
-            maxSoFar = max(dp[i], maxSoFar)
-
-        return maxSoFar
-        
