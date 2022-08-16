ad6f81de5885bfdf2107c6d216424a3e536ec7db
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-16 12:52:20 +0530

    Time: 7081 ms (5.01%), Space: 166.5 MB (17.64%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -1,12 +1,12 @@
-class Solution:
-    @cache
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+class Solution1:    
+    def longestPalindromeSubseq(self, s: str) -> int:
         # Solution - recursion with memorization
-        # Time - O(N * M)
-        #     - N = len of text1
-        #     - M = len of text2
-        # Space - O(N * M)
-        
+        # Time - O(N^2)
+        #     - N = len of s
+        # Space - O(N^2)
+        return self.longestCommonSubsequence(s, s[::-1])
+    
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:        
         dp = [[0] * (len(text2) + 1) for i in range(len(text1) + 1)]
         
         for i in range(1, len(text1) + 1):
@@ -18,6 +18,36 @@ class Solution:
         
         return dp[-1][-1]
     
+class Solution:    
     def longestPalindromeSubseq(self, s: str) -> int:
+        # Solution - recursion with memorization
+        # Time - O(N^2)
+        #     - N = len of s
+        # Space - O(N^2)
         return self.longestCommonSubsequence(s, s[::-1])
+    
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # Solution - recursion with memorization
+        # Time - O(N * M)
+        #     - N = len of text1
+        #     - M = len of text2
+        # Space - O(N * M)
+        
+        self.memo = {}
+        return self.recur(0, 0, text1, text2)
+    
+    def recur(self, i, j, text1, text2):
+        if i >= len(text1) or j >= len(text2):
+            return 0
+        
+        if (i, j) in self.memo:
+            return self.memo[(i, j)]
+        
+        if text1[i] == text2[j]:
+            self.memo[(i, j)] = self.recur(i+1, j+1, text1, text2) + 1
+        else:
+            self.memo[(i, j)] = max(self.recur(i, j+1, text1, text2),
+                  self.recur(i+1, j, text1, text2))
+        
+        return self.memo[(i, j)]
         
