fc43dc2b470ed0aca84983dc51cd91b5e3549c2a
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-16 13:40:45 +0530

    Time: 403 ms (65.36%), Space: 15 MB (55.67%) - LeetHub

---
+++ 494-target-sum/494-target-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    
+    def recur(self, i, curSum, nums, target):
+        if i >= len(nums):
+            return 1 if curSum == target else 0
+        
+        if (i, curSum) in self.memo:
+            return self.memo[(i, curSum)]
+        
+        self.memo[(i, curSum)] = (self.recur(i+1, curSum + nums[i], nums, target) + 
+                self.recur(i+1, curSum - nums[i], nums, target))
+    
+        return self.memo[(i, curSum)]
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        self.memo = {}
+        return self.recur(0, 0, nums, target)
+        
