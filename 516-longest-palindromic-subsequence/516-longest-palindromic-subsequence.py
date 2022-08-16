ef67b86fd2350539b592510e072f1292b23364c7
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-16 12:53:03 +0530

    Time: 4706 ms (22.80%), Space: 39.1 MB (45.57%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -17,37 +17,4 @@ class Solution:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         
         return dp[-1][-1]
-    
-class Solution1:    
-    def longestPalindromeSubseq(self, s: str) -> int:
-        # Solution - recursion with memorization
-        # Time - O(N^2)
-        #     - N = len of s
-        # Space - O(N^2)
-        return self.longestCommonSubsequence(s, s[::-1])
-    
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # Solution - recursion with memorization
-        # Time - O(N * M)
-        #     - N = len of text1
-        #     - M = len of text2
-        # Space - O(N * M)
-        
-        self.memo = {}
-        return self.recur(0, 0, text1, text2)
-    
-    def recur(self, i, j, text1, text2):
-        if i >= len(text1) or j >= len(text2):
-            return 0
-        
-        if (i, j) in self.memo:
-            return self.memo[(i, j)]
-        
-        if text1[i] == text2[j]:
-            self.memo[(i, j)] = self.recur(i+1, j+1, text1, text2) + 1
-        else:
-            self.memo[(i, j)] = max(self.recur(i, j+1, text1, text2),
-                  self.recur(i+1, j, text1, text2))
-        
-        return self.memo[(i, j)]
         
