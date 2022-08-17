307a1b76ba7313ec9efb7016cc72ef2e2fb66900
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-17 15:27:41 +0530

    Time: 347 ms (22.87%), Space: 15 MB (5.03%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -1,15 +1,22 @@
 class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        # Solution - BFS - graph coloring
+        # Time - O(V+E)
+        # Space - O(V)
+        
+        nodeColors = {}
+        for i in range(len(graph)):
+            if not self.bfs(i, nodeColors, set(), graph):
+                return False
+        return True
     
     def bfs(self, cur, nodeColors, path, graph):
-        
         if cur in nodeColors:
             return True
-        
-        bfs = [(cur, 'A')]
-        
-        while bfs:
+        bfsList = [(cur, 'A')]
+        while bfsList:
             nxt = []
-            for i, color in bfs:
+            for i, color in bfsList:
                 if i in nodeColors:
                     if nodeColors[i] != color:
                         return False
@@ -17,13 +24,6 @@ class Solution:
                     nodeColors[i] = color
                     for child in graph[i]:
                         nxt.append((child, 'B' if color == 'A' else 'A'))
-            bfs = nxt
-        return True
-    
-    def isBipartite(self, graph: List[List[int]]) -> bool:
-        nodeColors = {}
-        for i in range(len(graph)):
-            if not self.bfs(i, nodeColors, set(), graph):
-                return False
+            bfsList = nxt
         return True
         
