b4db977901addc1337167f747f15f19e4a131027
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-17 15:42:09 +0530

    Time: 3094 ms (88.54%), Space: 92.6 MB (27.16%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.py
+++ 474-ones-and-zeroes/474-ones-and-zeroes.py
@@ -1,22 +1,4 @@
 class Solution:
-    
-    def recur(self, idx, ms, ns, m, n, countDigits):
-        if (idx, ms, ns) in self.memo:
-            return self.memo[(idx, ms, ns)]
-        
-        if idx >= len(countDigits):
-            return ms <= m and ns <= n
-        
-        if ms > m or ns > n:
-            return 0
-        
-        take = self.recur(idx+1, ms + countDigits[idx][0], ns + countDigits[idx][1], m, n, countDigits) + 1
-        dontTake = self.recur(idx+1, ms, ns, m, n, countDigits) 
-        
-        self.memo[(idx, ms, ns)] = max(take, dontTake)
-        
-        return self.memo[(idx, ms, ns)]
-    
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         countDigits = []
         self.memo = {}
@@ -32,7 +14,22 @@ class Solution:
         for i in range(len(strs)):
             countDigits.append(get01Count(strs[i]))
         
-        print(countDigits)
-            
-        return self.recur(0, 0, 0, m, n, countDigits)-1
+        return self.recur(0, m, n, countDigits)-1
+    
+    def recur(self, idx, m, n, countDigits):
+        if (idx, m, n) in self.memo:
+            return self.memo[(idx, m, n)]
+        
+        if idx >= len(countDigits):
+            return m >= 0 and n >= 0
+        
+        if m < 0 or n < 0:
+            return 0
+        
+        take = self.recur(idx+1, m - countDigits[idx][0], n - countDigits[idx][1], countDigits) + 1
+        dontTake = self.recur(idx+1, m, n, countDigits) 
+        
+        self.memo[(idx, m, n)] = max(take, dontTake)
+        
+        return self.memo[(idx, m, n)]
         
