9fa1b7ec5ee78857dabaead63d2ea51b0b96aa8d
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-17 17:25:28 +0530

    Time: 409 ms (60.14%), Space: 16.1 MB (53.49%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -1,18 +1,17 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp = []
-        for i in range(len(word1)+1):
-            dp.append([0] * (len(word2)+1))
-            dp[i][0] = i
         
-        for i in range(len(word2) + 1):
-            dp[0][i] = i
+        # Solution - dynamic programming
+        # Time = O(N * M) N = len(word1), M = len(word2)
+        # Space = O(N * M) N = len(word1), M = len(word2)
+        
+        dp = [[0] * (len(word2)+1) for _ in range(len(word1) + 1)]
         
         for row in range(1, len(word1)+1):
             for col in range(1, len(word2)+1):
                 if word1[row-1] == word2[col-1]:
-                    dp[row][col] = dp[row - 1][col - 1]
+                    dp[row][col] = dp[row - 1][col - 1] + 1
                 else:
-                    dp[row][col] = min(dp[row - 1][col], dp[row][col - 1]) + 1
+                    dp[row][col] = max(dp[row - 1][col], dp[row][col - 1])
                 
-        return dp[len(word1)][len(word2)]
+        return len(word1) + len(word2) - (2 * dp[-1][-1])
