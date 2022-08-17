7307fc6ccee2484adf84f2b1da097cce96f30a68
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-17 13:35:43 +0530

    Time: 3035 ms (30.46%), Space: 29.5 MB (36.28%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,17 +1,21 @@
 class Solution:            
     def canPartition(self, nums: List[int]) -> bool:
+        # Solution - DP
+        # Time - O(N * S)
+        #      - N = len(nums)
+        #      - S = sum(nums) / 2
+        # Space - O(N * S)
         
         target = sum(nums)/2
         if target != int(target):
             return False
+        
         target = int(target)
         dp = [[False] * (target+1) for _ in range(len(nums))]
         
-        for i in range(len(nums)):
-            dp[i][0] = True
+        for i in range(len(nums)): dp[i][0] = True
         
-        if nums[0] <= target:
-            dp[0][nums[0]] = True
+        if nums[0] <= target: dp[0][nums[0]] = True
         
         for i in range(1, len(nums)):
             for j in range(1, target+1):
@@ -22,18 +26,6 @@ class Solution:
                 dp[i][j] = take | dontTake
         
         return dp[-1][-1]
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
 
 class Solution1:            
     def canPartition(self, nums: List[int]) -> bool:
