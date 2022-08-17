9a0ba2fbe85aac170df1f2f65e600c348a99603a
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-17 10:58:34 +0530

    Time: 548 ms (81.20%), Space: 22 MB (80.07%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -1,5 +1,10 @@
 class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        # Solution - LCS - traversal
+        # Time - O(N*M)
+        #     - N = len(str1)
+        #     - M = len(str2)
+        # Space - O(N*M)
         
         dp = [[0] * (len(str2)+1)  for _ in range(len(str1)+1)]
         
@@ -13,6 +18,11 @@ class Solution:
         result = []
         i, j = len(str1), len(str2)
         
+        '''
+        traverse dp and take char which are same 1`s
+        and chosse the 1 we left in order
+        '''
+        
         while i > 0 and j > 0:
             if str1[i-1] == str2[j-1]:
                 result.append(str1[i-1])
@@ -25,8 +35,7 @@ class Solution:
                     result.append(str2[j-1])
                     i, j = i, j-1
                     
-        # print(result[::-1], i, j)
-        
+        # for case when one string is taken fully and left other
         if  i == 0 and j > 0:
             result.append(str2[:j])
         if j == 0 and i > 0:
