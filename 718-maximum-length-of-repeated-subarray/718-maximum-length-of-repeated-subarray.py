9e25b6894983821eeec3d9f48ae9e961b1cb820f
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-17 19:07:40 +0530

    Time: 7734 ms (22.37%), Space: 14 MB (86.01%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -1,5 +1,29 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        # Solution - Longest common sub string
+        # Time - O(N*M)
+        # Space - O(N*M)
+        
+        pdp = [0] * (len(nums2)+1)
+        
+        maxSoFar = 0
+        
+        for i in range(1, len(nums1)+1):
+            cdp = [0] * (len(nums2)+1)
+            for j in range(1, len(nums2)+1):
+                if nums1[i-1] == nums2[j-1]:
+                    cdp[j] = pdp[j-1] + 1
+                maxSoFar = max(maxSoFar, cdp[j])
+            pdp = cdp
+        return maxSoFar
+
+
+class Solution1:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        # Solution - Longest common sub string
+        # Time - O(N*M)
+        # Space - O(N*M)
+        
         dp = [[0] * (len(nums2)+1) for _ in range(len(nums1) + 1)] 
         
         maxSoFar = 0
