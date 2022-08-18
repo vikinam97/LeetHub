483628484ee53cdee6ed43efe5f82673a3e8917f
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-18 09:38:33 +0530

    Time: 641 ms (90.47%), Space: 31.7 MB (86.40%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -4,10 +4,8 @@ class Solution:
         # Time - O(NlogN)
         # Space - (N)
         
-        freqSet = defaultdict(int)
+        freqSet = Counter(arr)
         heap = []
-        for num in arr:
-            freqSet[num] += 1
         
         for num in freqSet:
             heap.append(-1 * freqSet[num])
@@ -15,7 +13,6 @@ class Solution:
         heapq.heapify(heap)
         
         count, size = 0, 0
-        
         while heap and size < (len(arr) // 2):
             size += -1 * heapq.heappop(heap)
             count += 1
