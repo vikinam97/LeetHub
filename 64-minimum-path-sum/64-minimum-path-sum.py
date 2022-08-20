f2b8fb9ef6491a77b8de8e6b94882b5db5be4054
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-20 15:23:58 +0530

    Time: 189 ms (28.71%), Space: 15.9 MB (30.82%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,4 +1,28 @@
-class Solution:
+class Solution:        
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        h, w = len(grid), len(grid[0])
+        dp = [[0] * w for _ in range(h)]
+        dp[0][0] = grid[0][0]
+        
+        for i in range(1, h):
+            dp[i][0] = dp[i-1][0] + grid[i][0]
+        for j in range(1, w):
+            dp[0][j] = dp[0][j-1] + grid[0][j]
+        
+        for i in range(1, h):
+            for j in range(1, w):
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+
+        return dp[-1][-1]
+
+class Solution1:        
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        # Solution - Recursion + Memoization
+        # Time - O(N*M)
+        # Space - O(N*M)
+        
+        self.memo = {}
+        return self.recur(0, 0, grid)
     
     def recur(self, i, j, grid):
         if i >= len(grid) or j >= len(grid[0]):
@@ -14,7 +38,3 @@ class Solution:
                   self.recur(i, j+1, grid)) + grid[i][j]
         
         return self.memo[(i, j)]
-        
-    def minPathSum(self, grid: List[List[int]]) -> int:
-        self.memo = {}
-        return self.recur(0, 0, grid)
