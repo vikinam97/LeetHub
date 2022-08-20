fd12399d23464073351da15de77ad1cd8c847045
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-20 16:51:37 +0530

    Time: 525 ms (94.95%), Space: 16.4 MB (87.27%) - LeetHub

--- 221-maximal-square/221-maximal-square.py
+++ 221-maximal-square/221-maximal-square.py
@@ -1,5 +1,34 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
+        # Solution - DP - Space Optimized - Tabulation
+        # Time - O(N*N)
+        # Space - O(N)
+        
+        h, w = len(matrix), len(matrix[0])
+        pdp = [0] * w
+        
+        maxSoFar = 0
+        for i in range(0, h):
+            cdp = [0] * w
+            for j in range(0, w):
+                if i == 0:
+                    cdp[j] = int(matrix[0][j])
+                elif j == 0:
+                    cdp[0] = int(matrix[i][0])
+                elif matrix[i][j] == "1":
+                    cdp[j] = min(pdp[j], cdp[j-1], pdp[j-1]) + 1
+                
+                maxSoFar = max(maxSoFar, cdp[j] * cdp[j])
+            pdp = cdp
+            
+        return maxSoFar
+
+class Solutio1:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        # Solution - DP - Tabulation
+        # Time - O(N*N)
+        # Space - O(N*N)
+        
         h, w = len(matrix), len(matrix[0])
         dp = [[0]*w for _ in range(h)]
         
