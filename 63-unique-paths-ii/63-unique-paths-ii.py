582c7ddcd14cf9dc49d5d2d0d55215f0cfaefa0d
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-20 15:52:26 +0530

    Time: 68 ms (49.59%), Space: 14 MB (75.12%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -1,4 +1,36 @@
 class Solution:
+    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        h, w = len(grid), len(grid[0])
+        
+        dp = [[0]*w for _ in range(h)]
+        dp[0][0] = 0 if grid[0][0] == 1 else 1
+        for i in range(h):
+            if grid[i][0] == 1:
+                break
+            dp[i][0] = 1
+            
+        for j in range(w):
+            if grid[0][j] == 1:
+                break
+            dp[0][j] = 1
+        
+        for i in range(1, h):
+            for j in range(1, w):
+                if grid[i][j] != 1:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        return dp[-1][-1]
+
+
+class Solution1:
+    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        # Solution - Recursion + Memoization
+        # Time - O(N*M)
+        # Space - O(N*M)
+        
+        h, w = len(grid), len(grid[0])
+        self.memo = {}
+        return self.recur(0, 0, grid, h, w)
     
     def recur(self, i, j, grid, h, w):
         if i >= h or j >= w:
@@ -17,9 +49,4 @@ class Solution:
                self.recur(i, j+1, grid, h, w))
             
         return self.memo[(i, j)]
-    
-    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
-        h, w = len(grid), len(grid[0])
-        self.memo = {}
-        return self.recur(0, 0, grid, h, w)
         
