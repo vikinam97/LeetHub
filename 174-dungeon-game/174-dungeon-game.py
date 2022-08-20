ee60809a3c971bd889cc4f4aa58d23941243499b
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-20 14:06:46 +0530

    Time: 96 ms (78.72%), Space: 15.3 MB (34.92%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.py
+++ 174-dungeon-game/174-dungeon-game.py
@@ -1,5 +1,38 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        # Solution - Dynamic Programming Tabulation
+        # Time - O(N*M)
+        # Space - O(M)
+        
+        h, w = len(dungeon), len(dungeon[0])
+        
+        pdp  = [0]*w
+        
+        for i in reversed(range(h)):
+            cdp = [0]*w
+            for j in reversed(range(w)):
+                
+                if i == h-1 and j == w-1:
+                    cdp[j] = min(dungeon[i][j], 0)
+                    continue
+                    
+                cost = float('-inf') + 1000
+                if i + 1 < h:
+                    cost = pdp[j]
+                if j + 1 < w:
+                    cost = max(cost, cdp[j+1])
+                
+                cdp[j] = min(0, cost + dungeon[i][j])
+            
+            pdp = cdp
+        return abs(pdp[0]) +1
+
+class Solution1:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        # Solution - Dynamic Programming Tabulation
+        # Time - O(N*M)
+        # Space - O(N*M)
+        
         h, w = len(dungeon), len(dungeon[0])
         
         dp  = [[0]*w for _ in range(h)]
@@ -20,10 +53,6 @@ class Solution:
                 dp[i][j] = min(0, cost + dungeon[i][j])
                 
         return abs(dp[0][0]) +1
-        
-        
-        
-        
 
 class Solution1:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
