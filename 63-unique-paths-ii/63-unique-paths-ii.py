9f1bffa751ee0ba625e8b120367697ed16f8d8cf
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-20 15:57:52 +0530

    Time: 33 ms (99.82%), Space: 14 MB (43.62%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -1,5 +1,35 @@
 class Solution:
     def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        # Solution - DP Tabulation
+        # Time - O(N*M)
+        # Space - O(N*M)
+        
+        h, w = len(grid), len(grid[0])
+        
+        pdp = [0]*w
+        pdp[0] = 0 if grid[0][0] == 1 else 1
+                    
+        for j in range(w):
+            if grid[0][j] == 1:
+                break
+            pdp[j] = 1
+        
+        for i in range(1, h):
+            cdp = [0] * w
+            cdp[0] = 1 if pdp[0] == 1 and grid[i][0] == 0 else 0 
+            for j in range(1, w):
+                if grid[i][j] != 1:
+                    cdp[j] = pdp[j] + cdp[j-1]
+            pdp = cdp
+            
+        return pdp[-1]
+
+class Solution1:
+    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        # Solution - DP Tabulation
+        # Time - O(N*M)
+        # Space - O(N*M)
+        
         h, w = len(grid), len(grid[0])
         
         dp = [[0]*w for _ in range(h)]
