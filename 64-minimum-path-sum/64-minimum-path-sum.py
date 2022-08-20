769ebd2dfed2414c82dfd8a1a5a8d08026a76f5b
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-20 15:27:22 +0530

    Time: 157 ms (55.25%), Space: 14.7 MB (91.58%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,5 +1,31 @@
 class Solution:        
     def minPathSum(self, grid: List[List[int]]) -> int:
+        # Solution - DP Tabulation
+        # Time - O(N*M)
+        # Space - O(M)
+        
+        h, w = len(grid), len(grid[0])
+        pdp = [0] * w
+        pdp[0] = grid[0][0]
+        
+        for j in range(1, w):
+            pdp[j] = pdp[j-1] + grid[0][j]
+        
+        for i in range(1, h):
+            cdp = [0] * w
+            cdp[0] = pdp[0] + grid[i][0]
+            for j in range(1, w):
+                cdp[j] = min(pdp[j], cdp[j-1]) + grid[i][j]
+            
+            pdp = cdp
+        return pdp[-1]
+
+class Solution1:        
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        # Solution - DP Tabulation
+        # Time - O(N*M)
+        # Space - O(N*M)
+        
         h, w = len(grid), len(grid[0])
         dp = [[0] * w for _ in range(h)]
         dp[0][0] = grid[0][0]
