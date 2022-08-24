7bed3bd9e650a7bf8033d98a4591623800fa4920
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-24 11:53:21 +0530

    Time: 93 ms (85.84%), Space: 13.8 MB (57.97%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -1,5 +1,12 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
+        if n < 1:
+            return False
+        log = math.log10(n) / math.log10(3)
+        return log == int(log)
+
+class Solution1:
+    def isPowerOfThree(self, n: int) -> bool:
         
         while n > 1:
             n = n / 3
