edab02a480562531ad3ec33d94bee791e20e983e
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-24 15:25:03 +0530

    Time: 835 ms (11.58%), Space: 20.1 MB (86.32%) - LeetHub

--- 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.py
+++ 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.py
@@ -3,20 +3,16 @@ MOD = 10**9+7
 class Solution:
     
     def recur(self, i, mask, hatMap, n):
+        if mask == (1 << n) -1: return 1
+        if i > 40: return 0
         
-        if (i, mask) in self.memo:
+        if (i, mask) in self.memo: 
             return self.memo[(i, mask)]
-        
-        if mask == (1 << n) -1:
-            return 1
-        
-        if i >= 40:
-            return 0
         
         count = 0
         count = count + self.recur(i+1, mask, hatMap, n)
         
-        for ppl in hatMap[i+1]:
+        for ppl in hatMap[i]:
             if mask & (1 << ppl) == 0: 
                 count = count + self.recur(i+1, mask | (1 << ppl), hatMap, n)
         
