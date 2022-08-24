177f539186e385cd267cd9766c69c19e098add1d
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-24 15:19:35 +0530

    Time: 913 ms (10.53%), Space: 20 MB (86.32%) - LeetHub

--- 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.py
+++ 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.py
@@ -1,26 +1,26 @@
-class Solution:
-    def numberWays(self, hats: List[List[int]]) -> int:
-        n = len(hats)
-        MOD = 10**9 + 7
-        hat2ppl = defaultdict(list)
-        for i, hat in enumerate(hats):
-            for x in hat:
-                hat2ppl[x] += i,
-        memo = defaultdict(int)
+# class Solution:
+#     def numberWays(self, hats: List[List[int]]) -> int:
+#         n = len(hats)
+#         MOD = 10**9 + 7
+#         hat2ppl = defaultdict(list)
+#         for i, hat in enumerate(hats):
+#             for x in hat:
+#                 hat2ppl[x] += i,
+#         memo = defaultdict(int)
 
-        def dp(h, mask):
-            if mask == (1 << n) - 1: 
-                return 1
-            if h == 40:
-                return 0
-            if (h, mask) not in memo:
-                memo[h, mask] = dp(h+1, mask)
-                for p in hat2ppl[h+1]:
-                    if mask & (1<<p) == 0:
-                        memo[h, mask] += dp(h+1, mask | (1 << p))
-            return memo[h, mask] % MOD
+#         def dp(h, mask):
+#             if mask == (1 << n) - 1: 
+#                 return 1
+#             if h == 40:
+#                 return 0
+#             if (h, mask) not in memo:
+#                 memo[h, mask] = dp(h+1, mask)
+#                 for p in hat2ppl[h+1]:
+#                     if mask & (1<<p) == 0:
+#                         memo[h, mask] += dp(h+1, mask | (1 << p))
+#             return memo[h, mask] % MOD
         
-        return dp(0, 0)
+#         return dp(0, 0)
 
 
 
@@ -31,7 +31,7 @@ class Solution:
 
 MOD = 10**9+7
 
-class Solution1:
+class Solution:
     
     def recur(self, i, mask, hatMap, n):
         
@@ -47,7 +47,7 @@ class Solution1:
         count = 0
         count = (count + (self.recur(i+1, mask, hatMap, n) % MOD)) % MOD
         
-        for ppl in hatMap[i]:
+        for ppl in hatMap[i+1]:
             if mask & (1 << ppl) == 0: 
             
                 mask = mask | (1 << ppl)
