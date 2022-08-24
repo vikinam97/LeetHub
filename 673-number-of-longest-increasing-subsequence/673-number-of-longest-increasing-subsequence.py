396bd5d7ea229532b560f6e277b0511a64cf83f7
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-24 20:05:55 +0530

    Time: 1182 ms (83.55%), Space: 14.1 MB (72.82%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.py
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.py
@@ -1,5 +1,9 @@
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
+        # Solution - LIS Iterative
+        # Time - O(N^2)
+        # Space - O(N)
+        
         dp = [1] * len(nums)
         cnt = [1] * len(nums)
         
@@ -8,11 +12,13 @@ class Solution:
         for i in range(len(nums)):
             for j in range(i):
                 if nums[j] < nums[i]:
+                    
                     if dp[i] < dp[j] + 1:
                         dp[i] = dp[j] + 1
                         cnt[i] = cnt[j]
                     elif dp[i] == dp[j] + 1:
                         cnt[i] += cnt[j]
+                        
             maxSoFar = max(maxSoFar, dp[i])
             
         nos = 0
