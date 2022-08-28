66f32c48efe8c9bda6c61d8b22b0f4201fbad1dc
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-08-28 11:39:20 +0530

    Time: 86 ms (93.82%), Space: 14.3 MB (51.34%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -5,14 +5,18 @@ class Solution:
         # Space - O(1)
         
         n, m = len(A), len(A[0])
-        d = collections.defaultdict(list)
+        hashMap = defaultdict(list)
+        
         for i in range(n):
             for j in range(m):
-                d[i - j].append(A[i][j])
-        for k in d:
-            d[k].sort(reverse=1)
+                hashMap[i - j].append(A[i][j])
+                
+        for key in hashMap:
+            hashMap[key].sort(reverse=True)
+            
         for i in range(n):
             for j in range(m):
-                A[i][j] = d[i - j].pop()
+                A[i][j] = hashMap[i - j].pop()
+                
         return A
         
