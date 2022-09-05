e25fee1402fa40a85b3e7cb60128b2bb476a9602
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-05 12:43:47 +0530

    Time: 2085 ms (50.00%), Space: 88 MB (70.00%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
@@ -1,6 +1,15 @@
 MOD = (10**9) + 7
 
 class Solution:
+    def numberOfWays(self, startPos: int, endPos: int, k: int) -> int:
+        # Solution - Recursion + Memoization
+        # Time - O(K * (S-E))
+        # Space - O(K * (S-E))
+        
+        self.memo = {}
+        return self.recur(startPos, endPos, k) % MOD
+    
+    
     def recur(self, pos, end, k):
         if k == 0:
             return 1 if pos == end else 0
@@ -16,6 +25,3 @@ class Solution:
         
         return self.memo[(pos, k)]
     
-    def numberOfWays(self, startPos: int, endPos: int, k: int) -> int:
-        self.memo = {}
-        return self.recur(startPos, endPos, k) % MOD
