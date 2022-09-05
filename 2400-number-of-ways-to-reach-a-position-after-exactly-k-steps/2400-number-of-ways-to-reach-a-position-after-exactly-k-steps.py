9d176e840cdf69c187cdd2ef56d589f5a4cb494b
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-05 12:33:46 +0530

    Time: 2822 ms (40.00%), Space: 121.7 MB (60.00%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
@@ -1,7 +1,6 @@
 MOD = (10**9) + 7
 
 class Solution:
-    @cache
     def recur(self, pos, end, k):
         if k == 0:
             return 1 if pos == end else 0
@@ -9,7 +8,14 @@ class Solution:
         if pos - end > k:
             return 0
         
-        return self.recur(pos + 1, end, k-1) + self.recur(pos - 1, end, k - 1)
+        if (pos, k) in self.memo:
+            return self.memo[(pos, k)]
+        
+        self.memo[(pos, k)] = (self.recur(pos + 1, end, k-1) + 
+                               self.recur(pos - 1, end, k - 1))
+        
+        return self.memo[(pos, k)]
     
     def numberOfWays(self, startPos: int, endPos: int, k: int) -> int:
+        self.memo = {}
         return self.recur(startPos, endPos, k) % MOD
