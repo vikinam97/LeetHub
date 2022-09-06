bd498f5824783056eb5d704e35d2dd3a5349214b
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-06 10:10:38 +0530

    Time: 41 ms (74.34%), Space: 13.9 MB (68.92%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.py
+++ 814-binary-tree-pruning/814-binary-tree-pruning.py
@@ -1,5 +1,15 @@
 
 class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # Solution - DFS traversal
+        # Time - O(N)
+        # Space - O(N)
+        
+        v = self.recur(root)
+        if not v and not root.val:
+            return None
+        
+        return root
     
     def recur(self, node):
         if not node: return
@@ -13,14 +23,6 @@ class Solution:
             node.right = None
         
         return node.val or l or r
-        
-    
-    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        v = self.recur(root)
-        if not v and not root.val:
-            return None
-        
-        return root
         
         
         
