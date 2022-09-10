edb3cf49fc0ddcfc0e472f1ffd6a5952c48f6b82
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-10 16:55:23 +0530

    Time: 186 ms (5.09%), Space: 15.4 MB (9.72%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,5 +1,57 @@
+class Solution0:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Solution - Greedy - buy at valley and sell at peak
+        # Time - O(N)
+        # Space - O(1)
+        
+        profit = 0
+        i = 0
+        while i < len(prices):
+            j = i
+            while j < len(prices)-1 and prices[j] < prices[j+1]:
+                j += 1
+            profit += (prices[j] - prices[i])
+            i = j + 1
+        
+        return profit
+    
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Solution - DP - iterative - Space Optimized
+        # Time - O(N*2)
+        #     - N = len(prices)
+        # Space - O(N*2)
+        
+        n = len(prices)
+        pdp = [0] * 2
+        
+        pdp[0] = pdp[1] = 0
+        
+        for i in reversed(range(n)):
+            cdp = [0] * 2
+            for canBuy in range(2):
+                result = float('-inf')
+        
+                if canBuy:
+                    result = max(-prices[i] + pdp[0],
+                                pdp[1])
+                else:
+                    result = max(prices[i] + pdp[1],
+                                pdp[0])
+                
+                cdp[canBuy] = result
+            
+            pdp = cdp
+        
+        return pdp[1]
+    
 class Solution1:
     def maxProfit(self, prices: List[int]) -> int:
+        # Solution - DP - iterative
+        # Time - O(N*2)
+        #     - N = len(prices)
+        # Space - O(N*2)
+        
         n = len(prices)
         dp = [[0] * 2 for _ in range(n + 1)]
         
@@ -19,32 +71,6 @@ class Solution1:
                 dp[i][canBuy] = result
         
         return dp[0][1]
-                
-        
-        
-        
-        
-        
-        
-        
-
-
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        # Solution - buy at valley and sell at peak
-        # Time - O(N)
-        # Space - O(1)
-        
-        profit = 0
-        i = 0
-        while i < len(prices):
-            j = i
-            while j < len(prices)-1 and prices[j] < prices[j+1]:
-                j += 1
-            profit += (prices[j] - prices[i])
-            i = j + 1
-        
-        return profit
 
 class Solution2:
     def maxProfit(self, prices: List[int]) -> int:
