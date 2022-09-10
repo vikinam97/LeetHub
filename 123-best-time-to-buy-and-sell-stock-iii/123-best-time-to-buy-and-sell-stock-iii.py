c00c90edc03c73a978a2373ae0c0bdf958e29c1e
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-10 17:13:28 +0530

    Time: 7548 ms (5.01%), Space: 53.7 MB (24.45%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,5 +1,43 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[[0] * 3 for h in range(2)] for _ in range(n + 1)]
+        
+        dp[n][0][2] = dp[n][1][2] = 0
+        
+        for i in reversed(range(n)):
+            for canBuy in reversed(range(2)):
+                for k in reversed(range(1, 2+1)):
+                    result = float('-inf')
+        
+                    if canBuy:
+                        result = max(-prices[i] + dp[i+1][0][k],
+                                    dp[i+1][1][k])
+                    else:
+                        result = max(prices[i] + dp[i+1][1][k-1],
+                                    dp[i+1][0][k])
+                    
+                    dp[i][canBuy][k] = result
+        
+        return dp[0][1][2]
+        
+                    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+class Solution1:
+    def maxProfit(self, prices: List[int]) -> int:
         # Solution - Recursion + Memoization
         # Time - O(N*2*K)
         #     - N = len(prices)
@@ -28,4 +66,3 @@ class Solution:
             
         self.memo[(i, k, canBuy)] = result
         return result
-    
