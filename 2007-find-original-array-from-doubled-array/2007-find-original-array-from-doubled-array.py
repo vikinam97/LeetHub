32f58c157bef2d0b53e1f57456435cb7a8a586e5
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-15 11:58:01 +0530

    Time: 4914 ms (5.13%), Space: 34.6 MB (10.25%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        t = len(changed) / 2
+        if t != int(t):
+            return []
+        
+        changed.sort()
+        hashMap = defaultdict(list)
+        for i in range(len(changed)):
+            hashMap[changed[i]].append(i)
+        
+        result = []
+        
+        for i in range(len(changed)):
+            if changed[i] == -1:
+                continue
+                
+            if not len(hashMap[changed[i] * 2]):
+                return []
+            
+            tar = hashMap[changed[i] * 2].pop()
+            
+            if tar < i:
+                return []
+            
+            changed[tar] = -1            
+            result.append(changed[i])
+        
+        return result
+        
