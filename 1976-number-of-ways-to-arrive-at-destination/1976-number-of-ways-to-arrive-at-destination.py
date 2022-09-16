9b8832ca288370dd20100d9d268af5c1cfc091ba
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-16 15:57:54 +0530

    Time: 668 ms (22.52%), Space: 19.7 MB (100.00%) - LeetHub

---
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
@@ -0,0 +1,35 @@
+
+MOD = (10**9) + 7
+
+class Solution:
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        adjMat = defaultdict(list)
+        
+        for u, v, dist in roads:
+            adjMat[u].append((v, dist))
+            adjMat[v].append((u, dist))
+        
+        dist = [float('inf')] * n
+        dist[0] = 0
+        visited = [0] * n
+        
+        ways = [0] * n
+        ways[0] = 1
+        
+        heap = [(0, 0)] # (distance, vertex)
+        
+        while heap:
+            distance, node = heapq.heappop(heap)
+            
+            if visited[node]: continue
+            
+            for adj, dv in adjMat[node]:
+                if dv + distance < dist[adj]:
+                    dist[adj] = dv + distance
+                    heapq.heappush(heap, (dv + distance, adj))
+                    ways[adj] = ways[node]
+                elif dist[adj] == dv + distance:
+                    ways[adj] += ways[node]
+
+        return ways[n-1] % MOD
+        
