f123f19de0ceeed0dbad6b1fc3bd2bce8624469e
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-16 15:59:09 +0530

    Time: 315 ms (97.97%), Space: 20.4 MB (72.97%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
@@ -5,28 +5,27 @@ class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
         adjMat = defaultdict(list)
         
-        for u, v, dist in roads:
-            adjMat[u].append((v, dist))
-            adjMat[v].append((u, dist))
+        for u, v, t in roads:
+            adjMat[u].append((v, t))
+            adjMat[v].append((u, t))
         
-        dist = [float('inf')] * n
-        dist[0] = 0
-        visited = [0] * n
+        times = [float('inf')] * n
+        times[0] = 0
         
         ways = [0] * n
         ways[0] = 1
         
-        heap = [(0, 0)] # (distance, vertex)
+        heap = [(0, 0)] # (time, vertex)
         
         while heap:
             distance, node = heapq.heappop(heap)
                         
             for adj, dv in adjMat[node]:
-                if dv + distance < dist[adj]:
-                    dist[adj] = dv + distance
+                if dv + distance < times[adj]:
+                    times[adj] = dv + distance
                     heapq.heappush(heap, (dv + distance, adj))
                     ways[adj] = ways[node]
-                elif dist[adj] == dv + distance:
+                elif times[adj] == dv + distance:
                     ways[adj] += ways[node]
 
         return ways[n-1] % MOD
