fd4c53f1d9f79c050a2c5129c2f2415581823691
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-16 11:27:28 +0530

    Time: 3345 ms (99.82%), Space: 23.3 MB (97.48%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
@@ -1,5 +1,28 @@
 class Solution:
     def maximumScore(self, nums: List[int], muls: List[int]) -> int:
+        # Solution - DP - Space Optimized
+        # Time - O(M*M)
+        # Space - O(M)
+        n, m = len(nums), len(muls)
+        
+        pdp = [0]*(m+1)
+        
+        for i in range(m - 1, -1, -1):
+            cdp = [0]*(m+1)
+            for s in range(i, -1, -1):
+                cdp[s] = max(
+                    (muls[i]*nums[s]) + pdp[s+1],
+                    (muls[i]*nums[n - 1 - (i - s)]) + pdp[s])
+            pdp = cdp
+                
+        return pdp[0]
+
+class Solution1:
+    def maximumScore(self, nums: List[int], muls: List[int]) -> int:
+        # Solution - Iterative DP
+        # Time - O(M*M)
+        # Space - O(M*M)
+        
         n, m = len(nums), len(muls)
         
         dp = [[0]*(m+1) for _ in range(m+1)]
@@ -12,7 +35,7 @@ class Solution:
                 
         return dp[0][0]
     
-class Solution1:
+class Solution2:
     def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
         # Solution - TLE - Recusion + Memoization
         # Time - O(M*M)
