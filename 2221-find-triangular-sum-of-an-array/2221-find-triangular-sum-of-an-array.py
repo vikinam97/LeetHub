3d06918444190fdbeec9f204f226ee53e07e3dbe
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-17 18:19:20 +0530

    Time: 3272 ms (70.27%), Space: 13.9 MB (95.53%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.py
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.py
@@ -1,5 +1,8 @@
 class Solution:
     def triangularSum(self, nums: List[int]) -> int:
+        # Solution - process simulation
+        # Time - O(N^2)
+        # Space - O(N)
         
         for i in range(1, len(nums)):
             for j in range(len(nums)-i):
