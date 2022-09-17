a6aff651987df191adc8da8b80ee45bb2c0d527d
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-17 12:48:41 +0530

    Time: 78 ms (16.52%), Space: 14 MB (39.28%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
@@ -1,5 +1,15 @@
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
+
+        def get_key(log):
+            _id, rest = log.split(" ", maxsplit=1)
+            return (0, rest, _id) if rest[0].isalpha() else (1, )
+
+        return sorted(logs, key=get_key)
+
+
+class Solution1:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
         # Solution - parsing + sorting
         # Time - (NlogN + N + M)
         #     - N = len(ltrLogs)
