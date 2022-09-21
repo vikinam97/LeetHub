0fbe76c773231711cdc7e804205558ba0e67945b
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-21 15:41:07 +0530

    Time: 4037 ms (6.03%), Space: 29.9 MB (29.43%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,20 +1,22 @@
 class Solution:
     
     def partition(self, start, end, nums, k):
+        # Solution - Quick Select
+        # Time - O(N*N) worst - avg O(N)
+        # Space - O(1)
+        
         if start >= end:
             return nums[start]
         
         i, j, pivot = start, end, end
         
         while i < j:
-            
             if nums[i] < nums[pivot]:
                 i += 1
                 continue
             if nums[j] >= nums[pivot]:
                 j -= 1
                 continue
-            
             nums[i], nums[j] = nums[j], nums[i]
         
         nums[i], nums[pivot] = nums[pivot], nums[i]
@@ -27,9 +29,22 @@ class Solution:
         return nums[i]     
     
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        
         return self.partition(0, len(nums)-1, nums, k)
     
-    
+class Solution1:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # Solution - Heap
+        # Time - O(NlogK)
+        # Space - O(K)
+        
+        minHeap = []
+        
+        for i in range(len(nums)):
+            heapq.heappush(minHeap, nums[i])
+            
+            while len(minHeap) > k:
+                heapq.heappop(minHeap)
+            
+        return minHeap[0]
     
 
