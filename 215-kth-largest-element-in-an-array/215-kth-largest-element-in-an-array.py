891234bd0a29b85690639db32f1001456b9f65f6
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-21 15:19:17 +0530

    Time: 1305 ms (27.35%), Space: 27.2 MB (45.63%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,29 +1,13 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-                # Solution - Quick Select Algo
-        # Time - O(N)
-        # Space - O(log(k)) recursive stack
-        def partition(start, end):
-            i, j = start, end
-            pivot = end
-            
-            while i < j:
-                if nums[i] < nums[pivot]:
-                    i += 1
-                    continue
-                if nums[j] >= nums[pivot]:
-                    j -= 1
-                    continue
-                nums[i], nums[j] = nums[j], nums[i]
-                
-            nums[i], nums[pivot] = nums[pivot], nums[i]
-            
-            # selecting next apth
-            if i == len(nums) - k:
-                return nums[i]
-            elif i > len(nums) - k:
-                return partition(start, i-1)
-            
-            return partition(i+1, end)
-        return partition(0, len(nums) -1)
         
+        minHeap = []
+        
+        for i in range(len(nums)):
+            
+            heapq.heappush(minHeap, nums[i])
+            
+            while len(minHeap) > k:
+                heapq.heappop(minHeap)
+            
+        return minHeap[0]
