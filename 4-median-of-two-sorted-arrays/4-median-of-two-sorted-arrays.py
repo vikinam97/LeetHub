5d991469ec605a3d34bfb4986fbe7e68f09265d0
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-23 17:39:42 +0530

    Time: 102 ms (89.33%), Space: 14.1 MB (67.16%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,39 +1,35 @@
 class Solution:
     def findMedianSortedArrays(self, A: List[int], B: List[int]) -> float:
         
-        # Solution - binary search to partition list
-        # Time O(log(min(A, B)))
-        # Space O(1)
+        total = len(A) + len(B)
+        half = total // 2
         
-        totalSize = len(A) + len(B)
-        half = totalSize // 2
-        
-        # ensuring the A is smaller
-        if len(B) < len(A):
+        if len(A) > len(B):
             A, B = B, A
             
-        l, r = 0, len(A) - 1
+        l, r = 0, len(A) -1
         
         while True:
             
-            i = (l + r) // 2
+            i = l + ((r - l) // 2)
             j = half - (i+1) - 1
             
-            Aleft = A[i] if i >= 0 else float('-infinity')
-            Aright = A[i + 1] if (i+1) < len(A) else float("infinity")
-            Bleft = B[j] if j >= 0 else float('-infinity')
-            Bright = B[j + 1] if (j+1) < len(B) else float('infinity')
+            aLeft = A[i] if i >= 0 else float('-inf')
+            aRight = A[i+1] if i+1 < len(A) else float('inf')
+            bLeft = B[j] if j >= 0 else float('-inf')
+            bRight = B[j+1] if j+1 < len(B) else float('inf')
             
-            if Aleft <= Bright and Bleft <= Aright:
-                if totalSize % 2 == 0:
-                    return (min(Aright, Bright) + max(Aleft, Bleft)) / 2
+            if aLeft <= bRight and bLeft <= aRight:
+                if total & 1:
+                    return min(aRight, bRight)
                 else:
-                    return min(Aright, Bright)
-            elif Aleft > Bright:
-                r = i - 1
-            else:
-                l = i + 1
+                    return (min(aRight, bRight) + max(bLeft, aLeft)) / 2
             
+            if aLeft < bRight:
+                l = i + 1
+            else:
+                r = i - 1
             
             
         
+            
