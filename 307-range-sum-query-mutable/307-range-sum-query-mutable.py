6ff20c238b925baf9b8073d1a8d451e7ffd8fd93
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-24 15:55:04 +0530

    Time: 5634 ms (12.68%), Space: 33.3 MB (27.08%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.py
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.py
@@ -1,63 +1,57 @@
 class NumArray:
-    
-    def generateTree(self, nums, l, r, idx):
-        if l == r:
-            self.segmentTree[idx] = nums[l]
-            return nums[l]
-        
-        mid = l + ((r - l) // 2)
-        
-        lv = self.generateTree(nums, l, mid, 2 * idx)
-        rv = self.generateTree(nums, mid+1, r, (2 * idx) + 1)
-        
-        self.segmentTree[idx] = lv + rv
-        
-        return self.segmentTree[idx]
 
+    
     def __init__(self, nums: List[int]):
+        
+        self.tree = [0]*(4*len(nums))
         self.nums = nums
-        self.segmentTree = [0] * (4 * len(nums))
-        self.generateTree(nums, 0, len(nums)-1, 1)
+        
+        def generateTree(s, e, idx, nums):
+            if s == e:
+                self.tree[idx] = nums[s]
+                return nums[s]
+            mid = s + ((e - s) // 2)
+            l = generateTree(s, mid, 2 *idx, nums)
+            r = generateTree(mid+1, e, 2*idx+1, nums)
+
+            self.tree[idx] = l + r
+            return self.tree[idx]
+        
+        generateTree(0, len(nums)-1, 1, nums)
 
     def updateRecur(self, idx, l, r, pos, val):
-        # print(l, r)
         if pos > r or pos < l:
-            return self.segmentTree[idx]
+            return self.tree[idx]
         if l == r:
-            # print(l, r, idx, self.segmentTree[idx])
-            self.segmentTree[idx] = val
-            # print(idx, self.segmentTree[idx])
-            return self.segmentTree[idx]
+            self.tree[idx] = val
+            return self.tree[idx]
         
         mid = l + ((r - l ) // 2)
         lv = self.updateRecur(2 * idx, l, mid, pos, val)
         rv = self.updateRecur((2 * idx) + 1, mid + 1, r, pos, val)
         
-        self.segmentTree[idx] = lv + rv
-        return self.segmentTree[idx]
+        self.tree[idx] = lv + rv
+        return self.tree[idx]
         
     def update(self, index: int, val: int) -> None:
         self.updateRecur(1, 0, len(self.nums)-1, index, val)
-        # print(self.segmentTree)
+        
 
-    def query(self, idx, l, r, ql, qr):
-        if ql > r or qr < l:
-            return 0
-        if l >= ql and r <= qr:
-            return self.segmentTree[idx]
-        
-        mid = l + ((r - l ) // 2)
-        lv = self.query(2 * idx, l, mid, ql, qr)
-        rv = self.query((2 * idx) + 1, mid + 1, r, ql, qr)
-        
-        return lv + rv
-    
     def sumRange(self, left: int, right: int) -> int:
-        return self.query(1, 0, len(self.nums)-1, left, right)
         
-
-
-# Your NumArray object will be instantiated and called as such:
-# obj = NumArray(nums)
-# obj.update(index,val)
-# param_2 = obj.sumRange(left,right)
+        def query(idx, s, e, qs, qe):
+            if s > qe or e < qs:
+                return 0
+            if qs <= s and qe >= e:
+                return self.tree[idx]
+            
+            mid = s + ((e - s) // 2)
+            l = query(2*idx, s, mid, qs, qe)
+            r = query(2*idx + 1, mid+1, e, qs, qe)
+            
+            return l+r
+                        
+        return query(1, 0, len(self.nums)-1, left, right)
+    
+    
+        
