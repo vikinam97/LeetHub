1d4cc8454e161f171a49d7b7be003f66c15cf4c2
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-28 08:50:46 +0530

    Time: 242 ms (85.53%), Space: 16.3 MB (10.34%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.py
+++ 503-next-greater-element-ii/503-next-greater-element-ii.py
@@ -1,30 +1,23 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         
+        def traverseNextGreat():
+            i = 0
+            while i < len(nums):
+
+                while stack and stack[-1][0] < nums[i]:
+                    val, idx = stack.pop()
+                    nxtGrt[idx] = nums[i]
+
+                stack.append([nums[i], i])
+
+                i += 1
+                
         stack = []
         nxtGrt = [-1] * len(nums)
         
-        i = 0
-        while i < len(nums):
-            
-            while stack and stack[-1][0] < nums[i]:
-                val, idx = stack.pop()
-                nxtGrt[idx] = nums[i]
-            
-            stack.append([nums[i], i])
-        
-            i += 1
-            
-        i = 0
-        while i < len(nums):
-            
-            while stack and stack[-1][0] < nums[i]:
-                val, idx = stack.pop()
-                nxtGrt[idx] = nums[i]
-            
-            stack.append([nums[i], i])
-        
-            i += 1
+        traverseNextGreat()
+        traverseNextGreat()
         
         return nxtGrt
         
