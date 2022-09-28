b11900d4a4a594907cf9b9d1b600e438cb5b0ab1
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2022-09-28 09:41:33 +0530

    Time: 6073 ms (5.00%), Space: 39.8 MB (5.04%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,28 +1,35 @@
-class HeapNode:
-    def __init__(self, weight, val):
-        self.weight = weight
-        self.val = val
-    
-    def __lt__(self, other):
-        return self.weight > other.weight 
+from heapq import heapify, heappop, heappush
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        maxHeap = []
-        j = 0
         
-        result = []
-        for i in range(len(nums)):
-            heapq.heappush(maxHeap, HeapNode(nums[i], i))
+        heap = []
+        
+        i = 0
+        while i < k:
+            heap.append((-1*nums[i], i))
+            i += 1
+        
+        heapify(heap)
+        
+        winMax = [-1 * heap[0][0]]
+        
+        while i < len(nums):
             
-            if (i - j + 1) > k:
-                j += 1
-                
-            while maxHeap and maxHeap[0].val < j:
-                a = heapq.heappop(maxHeap)
-                
-            if (i - j + 1) == k:
-                result.append(maxHeap[0].weight)
-                
-        return result
+            heappush(heap, (-1*nums[i], i))
+            
+            while heap and heap[0][1] < i - k + 1:
+                heappop(heap)
+            
+            winMax.append(-1 * heap[0][0])
         
+            i += 1
+        
+        return winMax
+            
+            
+            
+            
+        
+                
+            
